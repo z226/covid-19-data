9404d28f5b5bc5a07b6624440724cc632e0dded7
lucas rg <lucasrg@kth.se>
2021-05-10 11:34:54 +0100

    temporary Malta fix

--- scripts/scripts/vaccinations/src/vax/batch/malta.py
+++ scripts/scripts/vaccinations/src/vax/batch/malta.py
@@ -39,6 +39,7 @@ def enrich_columns(df: pd.DataFrame) -> pd.DataFrame:
 def exclude_data_points(df: pd.DataFrame) -> pd.DataFrame:
     # The data contains an error that creates a negative change in the people_vaccinated series
     df = df[df.date.astype(str) != "2021-01-24"]
+    df = df[df.date.astype(str) >= "2021-05-05"]
     return df
 
 

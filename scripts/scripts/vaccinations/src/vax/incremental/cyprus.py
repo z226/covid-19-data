941f561b629dafe9fc7365e55a634677b01b7ac7
twobytescy <50107570+twobytescy@users.noreply.github.com>
2021-05-12 10:05:15 +0300

    Update cyprus.py
    Cyprus provides also AstraZeneca & J&J

--- scripts/scripts/vaccinations/src/vax/incremental/cyprus.py
+++ scripts/scripts/vaccinations/src/vax/incremental/cyprus.py
@@ -38,7 +38,7 @@ def enrich_location(ds: pd.Series) -> pd.Series:
 
 
 def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "vaccine", "Pfizer/BioNTech")
+    return enrich_data(ds, "vaccine", "Pfizer/BioNTech/AstraZeneca/Johnson&Johnson")
 
 
 def pipeline(ds: pd.Series) -> pd.Series:

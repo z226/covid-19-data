4b38276e9fd10c2a43f6f4f621d585208a6ad8c1
Edouard Mathieu <edmat@pm.me>
2021-05-13 13:47:44 +0200

    NorthMacedonia/vax: update vaccine list

--- scripts/scripts/vaccinations/src/vax/incremental/north_macedonia.py
+++ scripts/scripts/vaccinations/src/vax/incremental/north_macedonia.py
@@ -37,7 +37,9 @@ def enrich_location(ds: pd.Series) -> pd.Series:
 
 
 def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "vaccine", "Oxford/AstraZeneca, Pfizer/BioNTech, Sputnik V")
+    return enrich_data(
+        ds, "vaccine", "Oxford/AstraZeneca, Pfizer/BioNTech, Sinopharm/Beijing, Sputnik V"
+    )
 
 
 def enrich_source(ds: pd.Series, source: str) -> pd.Series:

a5e9331c942ff49ca854932d2e8acb9a7f56e406
lucas rg <lucasrg@kth.se>
2021-05-20 14:15:06 +0100

    Date fix, regex improvement

--- scripts/scripts/vaccinations/src/vax/incremental/equatorial_guinea.py
+++ scripts/scripts/vaccinations/src/vax/incremental/equatorial_guinea.py
@@ -1,92 +1,83 @@
-import os
-import datetime
 import re
-import requests
+import locale
+import unicodedata
+from datetime import datetime
 
-from bs4 import BeautifulSoup
 import pandas as pd
-import pytz
 
 from vax.utils.incremental import enrich_data, increment, clean_count
+from vax.utils.utils import get_soup
 
 
-def read(source: str) -> pd.Series:
-    return connect_parse_data(source)
+class EquatorialGuinea:
 
+    def __init__(self, source_url: str, location: str, columns_rename: dict = None):
+        self.source_url = source_url
+        self.location = location
 
-def connect_parse_data(source: str) -> pd.Series:
+    def read(self) -> pd.Series:
+        soup = get_soup(self.source_url)
+        people_vaccinated, people_fully_vaccinated = self.parse_vaccinated(soup)
+        date_str = self.parse_date(soup)
+        data = pd.Series({
+            "people_vaccinated": people_vaccinated,
+            "people_fully_vaccinated": people_fully_vaccinated,
+            "total_vaccinations": people_vaccinated + people_fully_vaccinated,
+            "date": self.parse_date(soup)
+        })
+        return pd.Series(data)
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:86.0) Gecko/20100101 Firefox/86.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        "Accept-Language": "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Connection": "keep-alive",
-        "Upgrade-Insecure-Requests": "1",
-        "Pragma": "no-cache",
-        "Cache-Control": "no-cache",
-    }
-    soup = BeautifulSoup(requests.get(source, headers=headers).content, "html.parser")
+    def parse_vaccinated(self, soup):
+        regex = r"De los ([\d\.]+) vacunados un total de ([\d\.]+) \(([\d\.]+)%\) ya han recibido la 2ª dosis"
+        match = re.search(regex, soup.text)
+        people_vaccinated = match.group(1)
+        people_fully_vaccinated = match.group(2)
+        return clean_count(people_vaccinated), clean_count(people_fully_vaccinated)
 
-    data = re.search(r"De los ([\d\.]+) vacunados un total de ([\d\.]+)", soup.text)
-    people_vaccinated = clean_count(data.group(1))
-    people_fully_vaccinated = clean_count(data.group(2))
+    def parse_date(self, soup):
+        regex = r"Datos: a  (\d{1,2}) ([a-zA-Z]+) de (202\d)"
+        locale.setlocale(locale.LC_TIME, "es_ES")
+        text = unicodedata.normalize('NFKC', soup.text)
+        match = re.search(regex, text)
+        return datetime.strptime(match.group(), "Datos: a  %d %B de %Y").strftime("%Y-%m-%d")
 
-    data = {
-        "people_vaccinated": people_vaccinated,
-        "people_fully_vaccinated": people_fully_vaccinated,
-    }
-    return pd.Series(data=data)
+    def pipe_location(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, 'location', self.location)
 
+    def pipe_vaccine(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, 'vaccine', "Sinopharm/Beijing")
 
-def add_totals(ds: pd.Series) -> pd.Series:
-    total_vaccinations = ds["people_vaccinated"] + ds["people_fully_vaccinated"]
-    return enrich_data(ds, "total_vaccinations", total_vaccinations)
+    def pipe_source(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, 'source_url', self.source_url)
 
+    def pipeline(self, ds: pd.Series) -> pd.Series:
+        return (
+            ds
+            .pipe(self.pipe_location)
+            .pipe(self.pipe_vaccine)
+            .pipe(self.pipe_source)
+        )
 
-def set_date(ds: pd.Series) -> pd.Series:
-    local_time = datetime.datetime.now(pytz.timezone("Africa/Malabo"))
-    local_time = local_time - datetime.timedelta(days=1)
-    date = str(local_time.date())
-    return enrich_data(ds, "date", date)
-
-
-def enrich_location(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "location", "Equatorial Guinea")
-
-
-def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "vaccine", "Sinopharm/Beijing")
-
-
-def enrich_source(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "source_url", "https://guineasalud.org/estadisticas/")
-
-
-def pipeline(ds: pd.Series) -> pd.Series:
-    return (
-        ds
-        .pipe(add_totals)
-        .pipe(set_date)
-        .pipe(enrich_location)
-        .pipe(enrich_vaccine)
-        .pipe(enrich_source)
-    )
+    def to_csv(self, paths):
+        """Generalized."""
+        data = self.read().pipe(self.pipeline)
+        increment(
+            paths=paths,
+            location=data['location'],
+            total_vaccinations=data['total_vaccinations'],
+            people_vaccinated=data['people_vaccinated'],
+            people_fully_vaccinated=data['people_fully_vaccinated'],
+            date=data['date'],
+            source_url=data['source_url'],
+            vaccine=data['vaccine']
+        )
 
 
 def main(paths):
-    source = "https://guineasalud.org/estadisticas/"
-    data = read(source).pipe(pipeline)
-    increment(
-        paths=paths,
-        location=data["location"],
-        total_vaccinations=data["total_vaccinations"],
-        people_vaccinated=data["people_vaccinated"],
-        people_fully_vaccinated=data["people_fully_vaccinated"],
-        date=data["date"],
-        source_url=data["source_url"],
-        vaccine=data["vaccine"]
-    )
+    EquatorialGuinea(
+        source_url="https://guineasalud.org/estadisticas/",
+        location="Equatorial Guinea",
+    ).to_csv(paths) 
 
 
 if __name__ == "__main__":

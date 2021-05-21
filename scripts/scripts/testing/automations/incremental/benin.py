44c2bb9e83b741c59f1370f5fd454bfe9c467422
Cam Appel <cameron@ourworldindata.org>
2021-05-21 16:34:52 -0400

    Update benin.py
    Add fail-safes

--- scripts/scripts/testing/automations/incremental/benin.py
+++ scripts/scripts/testing/automations/incremental/benin.py
@@ -5,26 +5,32 @@ import urllib.request
 import pandas as pd
 from bs4 import BeautifulSoup
 
-url = 'https://www.gouv.bj/coronavirus/'
-req = urllib.request.urlopen(url)
-soup = BeautifulSoup(req.read(), 'html.parser')
+def main():
+    
+    data = pd.read_csv("automated_sheets/Benin.csv")
 
-stats = soup.find_all('h2', attrs={'class', 'h1 adapt white regular'})
+    url = 'https://www.gouv.bj/coronavirus/'
+    req = urllib.request.urlopen(url)
+    soup = BeautifulSoup(req.read(), 'html.parser')
 
-count = int(stats[0].text) + int(stats[1].text)
-date_str = date.today().strftime("%Y-%m-%d")
-df = pd.DataFrame({
-    'Country': 'Benin',
-    'Date': [date_str],
-    'Cumulative total': count,
-    'Source URL': url,
-    'Source label': 'Government of Benin',
-    'Units': 'tests performed',
-})
+    stats = soup.find_all('h2', attrs={'class', 'h1 adapt white regular'})
 
-output_file = 'automated_sheets/Benin.csv'
-if os.path.isfile(output_file):
-    existing = pd.read_csv(output_file)
-    df = pd.concat([df, existing]).sort_values('Date', ascending=False)
+    count = int(stats[0].text) + int(stats[1].text)
+    date_str = date.today().strftime("%Y-%m-%d")
+    
+    if count > data["Cumulative total"].max() and date_str > data["Date"].max():
 
-df.to_csv(output_file, index=False)
+        new = pd.DataFrame({
+            'Country': 'Benin',
+            'Date': [date_str],
+            'Cumulative total': count,
+            'Source URL': url,
+            'Source label': 'Government of Benin',
+            'Units': 'tests performed',
+        })
+        
+        df = pd.concat([new, data], sort=False)
+        df.to_csv("automated_sheets/Benin.csv", index=False)
+
+if __name__ == '__main__':
+    main()

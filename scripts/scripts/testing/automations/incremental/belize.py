4bbaa9e688854b8e9611f3c80a56402295dcb8fa
Cam Appel <cameron@ourworldindata.org>
2021-05-21 16:08:25 -0400

    Update belize.py
    Edit metadata

--- scripts/scripts/testing/automations/incremental/belize.py
+++ scripts/scripts/testing/automations/incremental/belize.py
@@ -1,10 +1,8 @@
-import os
 import pandas as pd
 from bs4 import BeautifulSoup
-
+import requests
 from datetime import date
-import urllib.request
-
+import urllib
 
 url = 'https://sib.org.bz/covid-19/by-the-numbers/'
 req = urllib.request.urlopen(url)
@@ -13,19 +11,14 @@ soup = BeautifulSoup(req.read(), 'html.parser')
 stats = soup.select('div.stats-number.ult-responsive')
 count = int(stats[0]['data-counter-value'])
 
-date_str = date.today().strftime("%Y-%m-%d")
-df = pd.DataFrame({
-    'Country': 'Belize',
-    'Date': [date_str],
-    'Cumulative total': count,
-    'Source URL': url,
-    'Source label': 'Government of Suriname',
-    'Units': 'unclear',
-})
+date = str(date.today())
+new = pd.DataFrame({'Country': 'Belize',
+                   'Date': [date],
+                   'Cumulative total': count,
+                   'Source URL': url,
+                   'Source label': 'Statistical Institute of Belize',
+                   'Units': 'tests performed'})
 
-output_file = 'automated_sheets/Belize.csv'
-if os.path.isfile(output_file):
-    existing = pd.read_csv(output_file)
-    df = pd.concat([df, existing]).sort_values('Date', ascending=False)
-
-df.to_csv(output_file, index=False)
+existing = pd.read_csv('automated_sheets/Belize.csv')
+df = pd.concat([new,existing]).sort_values('Date',ascending=False)
+df.to_csv('automated_sheets/Belize.csv',index=False)

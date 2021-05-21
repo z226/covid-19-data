598f3b3ee02be596f7e1f0122389a711bc231b77
Cam Appel <cameron@ourworldindata.org>
2021-05-21 12:28:01 -0400

    Update belize.py
    Corrected metadata

--- scripts/scripts/testing/automations/batch/belize.py
+++ scripts/scripts/testing/automations/batch/belize.py
@@ -10,15 +10,14 @@ soup = BeautifulSoup(req.read(), 'html.parser')
 
 stats = soup.select('div.stats-number.ult-responsive')
 count = int(stats[0]['data-counter-value'])
-print(count)
 
 date = str(date.today())
 new = pd.DataFrame({'Country': 'Belize',
                    'Date': [date],
                    'Cumulative total': count,
                    'Source URL': url,
-                   'Source label': 'Government of Suriname',
-                   'Units': 'unclear'})
+                   'Source label': 'Statistical Institute of Belize',
+                   'Units': 'tests performed'})
 
 existing = pd.read_csv('automated_sheets/Belize.csv')
 df = pd.concat([new,existing]).sort_values('Date',ascending=False)

bcddcf20e355ffda3aef04dc31def66ff59ed938
lucas rg <lucasrg@kth.se>
2021-05-24 14:05:52 +0100

    VAX: Add second dose information Australia

--- scripts/scripts/vaccinations/src/vax/batch/australia.py
+++ scripts/scripts/vaccinations/src/vax/batch/australia.py
@@ -1,69 +1,80 @@
 import pandas as pd
 
 
-def read(source: str) -> pd.DataFrame:
-    return pd.read_json(source)
+class Australia:
 
+    def __init__(self, source_url: str, location: str, columns_rename: dict = None):
+        """Constructor.
 
-def filter_rows(df: pd.DataFrame) -> pd.DataFrame:
-    return df[(df.NAME == "Australia") & df.VACC_DOSE_CNT.notnull()]
+        Args:
+            source_url (str): Source data url
+            location (str): Location name
+            columns_rename (dict, optional): Maps original to new names. Defaults to None.
+        """
+        self.source_url = source_url
+        self.location = location
+        self.columns_rename = columns_rename
 
+    def read(self) -> pd.DataFrame:
+        return pd.read_json(self.source_url)
 
-def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
-    return df[[
-        "REPORT_DATE",
-        "VACC_DOSE_CNT",
-        # "VACC_PEOPLE_CNT"
-    ]].rename(
-        columns={
-            "REPORT_DATE": "date",
-            "VACC_DOSE_CNT": "total_vaccinations",
-            # "VACC_PEOPLE_CNT": "people_vaccinated",
-        }
-    )
+    def pipe_filter_rows(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df[(df.NAME == "Australia") & df.VACC_DOSE_CNT.notnull()]
 
+    def pipe_rename_columns(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df[self.columns_rename.keys()].rename(columns=self.columns_rename)
 
-def enrich_vaccinations(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.assign(
-        people_vaccinated=pd.NA,
-        people_fully_vaccinated=pd.NA,
-    )
-    return df
+    def pipe_people_full_vaccinated(self, df: pd.DataFrame) -> pd.DataFrame:
+        date_limit_low = "2021-03-15"
+        date_limit_up = "2021-05-24"
+        df.loc[(date_limit_low <= df.date) & (df.date < date_limit_up), "people_fully_vaccinated"] = pd.NA
+        df.loc[df.date < date_limit_low, "people_fully_vaccinated"] = 0
+        return df
 
+    def pipe_people_vaccinated(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df.assign(
+            people_vaccinated=df.total_vaccinations-df.people_fully_vaccinated,
+        )
+        return df
 
-def enrich_vaccine_name(df: pd.DataFrame) -> pd.DataFrame:
-    def _enrich_vaccine(date: str) -> str:
-        if date >= "2021-03-08":
-            return "Oxford/AstraZeneca, Pfizer/BioNTech"
-        return "Pfizer/BioNTech"
-    return df.assign(vaccine=df.date.astype(str).apply(_enrich_vaccine))
+    def pipe_vaccine(self, df: pd.DataFrame) -> pd.DataFrame:
+        def _enrich_vaccine(date: str) -> str:
+            if date >= "2021-03-08":
+                return "Oxford/AstraZeneca, Pfizer/BioNTech"
+            return "Pfizer/BioNTech"
+        return df.assign(vaccine=df.date.astype(str).apply(_enrich_vaccine))
 
+    def pipe_metadata(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.assign(
+            location="Australia",
+            source_url="https://covidlive.com.au/vaccinations"
+        )
 
-def enrich_columns(df: pd.DataFrame) -> pd.DataFrame:
-    return df.assign(
-        location="Australia",
-        source_url="https://covidlive.com.au/vaccinations"
-    )
+    def pipeline(self, df: pd.DataFrame) -> pd.DataFrame:
+        return (
+            df
+            .pipe(self.pipe_filter_rows)
+            .pipe(self.pipe_rename_columns)
+            .pipe(self.pipe_people_full_vaccinated)
+            .pipe(self.pipe_people_vaccinated)
+            .pipe(self.pipe_vaccine)
+            .pipe(self.pipe_metadata)
+            .sort_values("date")
+        )
 
-
-def pipeline(df: pd.DataFrame) -> pd.DataFrame:
-    return (
-        df
-        .pipe(filter_rows)
-        .pipe(rename_columns)
-        .pipe(enrich_vaccinations)
-        .pipe(enrich_vaccine_name)
-        .pipe(enrich_columns)
-        .sort_values("date")
-    )
+    def to_csv(self, paths):
+        """Generalized."""
+        df = self.read().pipe(self.pipeline)
+        df.to_csv(paths.tmp_vax_out(self.location), index=False)
 
 
 def main(paths):
-    source = "https://covidlive.com.au/covid-live.json"
-    destination = paths.tmp_vax_out("Australia")
-
-    read(source).pipe(pipeline).to_csv(destination, index=False)
-
-
-if __name__ == "__main__":
-    main()
+    Australia(
+        source_url="https://covidlive.com.au/covid-live.json",
+        location="Australia",
+        columns_rename={
+            "REPORT_DATE": "date",
+            "VACC_DOSE_CNT": "total_vaccinations",
+            "VACC_PEOPLE_CNT": "people_fully_vaccinated",
+        },
+    ).to_csv(paths)

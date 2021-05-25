823dcc3e92d8fc833f3403c325662bc2463e8e19
Cam Appel <cameron@ourworldindata.org>
2021-05-24 20:31:01 -0400

    Update belize.py
    Add fail-safes

--- scripts/scripts/testing/automations/incremental/belize.py
+++ scripts/scripts/testing/automations/incremental/belize.py
@@ -1,24 +1,36 @@
+import os
 import pandas as pd
 from bs4 import BeautifulSoup
-import requests
+
 from datetime import date
-import urllib
+import urllib.request
 
-url = 'https://sib.org.bz/covid-19/by-the-numbers/'
-req = urllib.request.urlopen(url)
-soup = BeautifulSoup(req.read(), 'html.parser')
+def main():
+    
+    data = pd.read_csv("automated_sheets/Belize.csv")
 
-stats = soup.select('div.stats-number.ult-responsive')
-count = int(stats[0]['data-counter-value'])
+    url = 'https://sib.org.bz/covid-19/by-the-numbers/'
+    req = urllib.request.urlopen(url)
+    soup = BeautifulSoup(req.read(), 'html.parser')
 
-date = str(date.today())
-new = pd.DataFrame({'Country': 'Belize',
-                   'Date': [date],
-                   'Cumulative total': count,
-                   'Source URL': url,
-                   'Source label': 'Statistical Institute of Belize',
-                   'Units': 'tests performed'})
+    stats = soup.select('div.stats-number.ult-responsive')
+    count = int(stats[0]['data-counter-value'])
+    print(count)
 
-existing = pd.read_csv('automated_sheets/Belize.csv')
-df = pd.concat([new,existing]).sort_values('Date',ascending=False)
-df.to_csv('automated_sheets/Belize.csv',index=False)
+    date = str(date.today())
+
+    if count > data["Cumulative total"].max() and date > data["Date"].max():
+        
+        new = pd.DataFrame({'Country': 'Belize',
+            'Date': [date],
+            'Cumulative total': count,
+            'Source URL': url,
+            'Source label': 'Statistical Institute of Belize',
+            'Units': 'tests performed',
+        })
+
+    df = pd.concat([new, data], sort=False)
+    df.to_csv("automated_sheets/Belize.csv", index=False)
+
+if __name__ == '__main__':
+    main()

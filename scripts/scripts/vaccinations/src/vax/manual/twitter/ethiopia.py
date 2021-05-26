82cc1fb5d30a5214872337713bb483b28f57d03e
lucas rg <lucasrg@kth.se>
2021-05-26 19:44:33 +0100

    Ethiopia

---
+++ scripts/scripts/vaccinations/src/vax/manual/twitter/ethiopia.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import re
+
+from vax.manual.twitter.base import TwitterCollectorBase
+from vax.utils.dates import clean_date
+
+
+class Ethiopia(TwitterCollectorBase):
+    def __init__(self, api):
+        super().__init__(
+            api=api,
+            username="FMoHealth",
+            location="Ethiopia",
+            add_metrics_nan=["total_vaccinations"],
+        )
+    
+    def _propose_df(self):
+        regex = r"ባለፉት 24 .*"
+        data = []
+        for tweet in self.tweets:
+            if re.search(regex, tweet.full_text):
+                data.append({
+                    "date": tweet.created_at.strftime("%Y-%m-%d"),
+                    "text": tweet.full_text,
+                    "source_url": self.build_post_url(tweet.id),
+                    "media_url": tweet.extended_entities["media"][1]["media_url_https"],
+                })
+        return pd.DataFrame(data)

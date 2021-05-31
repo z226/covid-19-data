b7c8430ca9aad06cde82e80c2e3aba25b80546b9
lucas rg <lucasrg@kth.se>
2021-05-31 13:01:27 +0100

    Saint Kitts and Nevis

--- scripts/scripts/testing/automations/incremental/saint-kitts-and-nevis.py
+++ scripts/scripts/testing/automations/incremental/saint-kitts-and-nevis.py
@@ -1,25 +1,37 @@
+import os
+from datetime import date
+
+import requests
 import pandas as pd
 from bs4 import BeautifulSoup
-import requests
-from datetime import date
-import urllib
 
-url = 'https://covid19.gov.kn/stats2.php'
-req = requests.get(url)
-soup = BeautifulSoup(req.text, 'html.parser')
 
-stats = soup.find_all('td')
-count = int(stats[7].text)
-print(count)
+def main():
+    url = 'https://covid19.gov.kn/stats2.php'
+    location = "Saint Kitts and Nevis"
+    output_file = f'automated_sheets/{location}.csv'
 
-date = str(date.today())
-new = pd.DataFrame({'Country': 'Saint Kitts and Nevis',
-                   'Date': [date],
-                   'Cumulative total': count,
-                   'Source URL': url,
-                   'Source label': 'Government of Saint Kitts and Nevis',
-                   'Units': 'unclear'})
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, 'html.parser')
+    stats = soup.find_all('td')
+    count = int(stats[7].text)
+    # print(count)
 
-existing = pd.read_csv('automated_sheets/saint-kitts-and-nevis.csv')
-df = pd.concat([new,existing]).sort_values('Date',ascending=False)
-df.to_csv('automated_sheets/saint-kitts-and-nevis.csv',index=False)
+    date_str = date.today().strftime("%Y-%m-%d")
+    df = pd.DataFrame({
+        'Country': location,
+        'Date': [date_str],
+        'Cumulative total': count,
+        'Source URL': url,
+        'Source label': 'Government of Saint Kitts and Nevis',
+        'Units': 'unclear'
+    })
+
+    if os.path.isfile(output_file):
+        existing = pd.read_csv(output_file)
+        df = pd.concat([df, existing]).sort_values('Date', ascending=False)
+    df.to_csv(output_file, index=False)
+
+
+if __name__=="__main__":
+    main()

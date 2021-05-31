2b87dbacbfe7da12ec2b564930df4ea17d142aaa
lucas rg <lucasrg@kth.se>
2021-05-31 12:44:43 +0100

    Fix Timor and add data

---
+++ scripts/scripts/testing/automations/incremental/timor.py
@@ -0,0 +1,37 @@
+import os
+from datetime import date
+
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+def main():
+    output_file = "automated_sheets/Timor.csv"
+    url = 'https://covid19.gov.tl/dashboard/'
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, 'html.parser')
+
+    stats = soup.find_all('span',attrs={'class': 'wdt-column-sum-value'})
+    count = int(stats[5].text.replace(',', ''))
+
+    date_str = date.today().strftime("%Y-%m-%d")
+    df = pd.DataFrame({
+        'Country': 'Timor',
+        'Date': [date_str],
+        'Cumulative total': count,
+        'Source URL': url,
+        'Source label': 'Government of Timor-Leste',
+        'Units': 'unclear'
+    })
+
+
+    if os.path.isfile(output_file):
+        existing = pd.read_csv(output_file)
+        df = pd.concat([df, existing]).sort_values('Date', ascending=False)
+    df.to_csv(output_file, index=False)
+
+
+
+if __name__ == "__main__":
+    main()

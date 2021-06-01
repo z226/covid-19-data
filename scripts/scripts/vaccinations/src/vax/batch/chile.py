c08d6919fca49ba7c5d69f9f90b922695c8a28d3
covid19owid <covid19owid@gmail.com>
2021-06-01 13:47:21 -0400

    Update chile.py
    Vaccination by age group has been updated for Chile

--- scripts/scripts/vaccinations/src/vax/batch/chile.py
+++ scripts/scripts/vaccinations/src/vax/batch/chile.py
@@ -11,12 +11,24 @@ def melt(df: pd.DataFrame) -> pd.DataFrame:
     return df.melt(["Type", "Dose"], var_name="date", value_name="value")
 
 
+def melt_by_age_group(df: pd.DataFrame) -> pd.DataFrame:
+    return df.melt(["Age", "Dose"], var_name="date", value_name="value")
+
+
 def filter_rows(df: pd.DataFrame) -> pd.DataFrame:
     return df[(df.Type != "Total") & (df.value > 0)]
 
 
+def filter_rows_by_age_group(df: pd.DataFrame) -> pd.DataFrame:
+    return df[(df.Age != "Total") & (df.value > 0)]
+
+
 def pivot(df: pd.DataFrame) -> pd.DataFrame:
     return df.pivot(index=["Type", "date"], columns="Dose", values="value").reset_index()
+
+
+def pivot_by_age_group(df: pd.DataFrame) -> pd.DataFrame:
+    return df.pivot(index=["Age", "date"], columns="Dose", values="value").reset_index()
 
 
 def enrich_vaccinations(df: pd.DataFrame) -> pd.DataFrame:
@@ -43,6 +55,16 @@ def preprocess(df: pd.DataFrame) -> pd.DataFrame:
         .pipe(pivot)
         .pipe(enrich_vaccinations)
         .pipe(rename_vaccines)
+    )
+
+
+def preprocess_by_age_group(df: pd.DataFrame) -> pd.DataFrame:
+    return (
+        df.pipe(melt_by_age_group)
+        .pipe(filter_rows_by_age_group)
+        .pipe(pivot_by_age_group)
+        .pipe(enrich_vaccinations)
+        .pipe(enrich_location)
     )
 
 
@@ -82,6 +104,11 @@ def postprocess_manufacturer(df: pd.DataFrame) -> pd.DataFrame:
     )
 
 
+def enrich_location(df: pd.DataFrame) -> pd.DataFrame:
+    """Generalized."""
+    return df.assign(location="Chile")
+
+
 def main(paths):
     source = "https://github.com/juancri/covid19-vaccination/raw/master/output/chile-vaccination-type.csv"
     data = read(source).pipe(preprocess)
@@ -96,6 +123,21 @@ def main(paths):
     )
     data.pipe(postprocess_manufacturer).to_csv(
         paths.tmp_vax_out_man(location),
+        index=False
+    )
+
+    # Vaccination data by age group
+    source_by_age_group = "https://raw.githubusercontent.com/juancri/covid19-vaccination/master/output/chile-vaccination-ages.csv"
+    data_by_age_group = read(source_by_age_group).pipe(preprocess_by_age_group)
+    data_by_age_group[["age_group_min", "age_group_max"]] = data_by_age_group.Age.apply(
+        lambda x: pd.Series(str(x).split("-")))
+    data_by_age_group = data_by_age_group[["date", "age_group_min", "age_group_max", "total_vaccinations", "location"]]
+    data_by_age_group = data_by_age_group.replace(to_replace='80 y más años', value='80', regex=True)
+    data_by_age_group = data_by_age_group.replace(to_replace='80 y más', value='80', regex=True)
+    data_by_age_group = data_by_age_group.replace(to_replace='90 y más', value='90', regex=True)
+    data_by_age_group = data_by_age_group.sort_values(by="date")
+    data_by_age_group.to_csv(
+        paths.tmp_vax_out_by_age_group(location),
         index=False
     )
 

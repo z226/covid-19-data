7d7a320bcd0b29dd7ea3f869a6f34f12e9634dea
lucas rg <lucasrg@kth.se>
2021-06-04 16:47:07 +0100

    Fix Sri Lanka

--- scripts/scripts/vaccinations/src/vax/incremental/sri_lanka.py
+++ scripts/scripts/vaccinations/src/vax/incremental/sri_lanka.py
@@ -6,92 +6,107 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import PyPDF2
 
-from vax.utils.incremental import enrich_data, increment, clean_count
+from vax.utils.incremental import increment
 from vax.utils.dates import clean_date
-
-def read(source: str) -> pd.Series:
-    soup = BeautifulSoup(requests.get(source).content, "html.parser")
-    return parse_data(soup)
+from vax.utils.utils import get_soup
 
 
-def parse_data(soup: BeautifulSoup) -> pd.Series:
-
-    # Get path to newest pdf
-    links = soup.find(class_="rt-article").find_all("a")
-    for link in links:
-        if "sitrep-sl-en" in link["href"]:
-            pdf_path = "https://www.epid.gov.lk" + link["href"]
-            break
-
-    tf = tempfile.NamedTemporaryFile()
-
-    with open(tf.name, mode="wb") as f:
-        f.write(requests.get(pdf_path).content)
-
-    with open(tf.name, mode="rb") as f:
-        reader = PyPDF2.PdfFileReader(f)
-        page = reader.getPage(0)
-        text = page.extractText().replace("\n", "")
-    
-    covishield_data = re.search(r"Covishield Vaccine +(\d+) (\d+)", text)
-    covishield_dose1 = clean_count(covishield_data.group(1))
-    covishield_dose2 = clean_count(covishield_data.group(2))
-
-    sinopharm_data = re.search(r"Sinopharm Vaccine +(\d+) (\d+)", text)
-    sinopharm_dose1 = clean_count(sinopharm_data.group(1))
-    sinopharm_dose2 = clean_count(sinopharm_data.group(2))
-    
-    sputnik_data = re.search(r"Sputnik V +(\d+) (-|\d+)", text)
-    sputnik_dose1 = clean_count(sputnik_data.group(1))
-    sputnik_dose2 = sputnik_data.group(2)
-    sputnik_dose2 = 0 if sputnik_dose2 == "-" else clean_count(sputnik_dose2)
-
-    total_vaccinations = covishield_dose1 + covishield_dose2 + sinopharm_dose1 + sinopharm_dose2 + sputnik_dose1 + sputnik_dose2
-    people_vaccinated = covishield_dose1 + sinopharm_dose1 + sputnik_dose1
-    people_fully_vaccinated = covishield_dose2 + sinopharm_dose2 + sputnik_dose2
-
-    regex = r"Situation Report\s+([\d\.]{10})"
-    date = re.search(regex, text).group(1)
-    date = clean_date(date, "%d.%m.%Y")
-
-    return pd.Series(data={
-        "total_vaccinations": total_vaccinations,
-        "people_vaccinated": people_vaccinated,
-        "people_fully_vaccinated": people_fully_vaccinated,
-        "date": date,
-        "source_url": pdf_path,
-    })
+vaccines_mapping = {
+    "Covishield Vaccine": "Oxford/AstraZeneca",
+    "Sinopharm Vaccine": "Sinopharm/Beijing",
+    "Sputnik V": "Sputnik V",
+}
 
 
-def enrich_location(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "location", "Sri Lanka")
+class SriLanka:
 
+    def __init__(self):
+        self.source_url = "https://www.epid.gov.lk/web/index.php?option=com_content&view=article&id=225&lang=en"
+        self.location = "Sri Lanka"
 
-def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "vaccine", "Oxford/AstraZeneca, Sinopharm/Beijing, Sputnik V")
+    def read(self):
+        soup = get_soup(self.source_url)
+        return self.parse_data(soup)
 
+    def parse_data(self, soup: BeautifulSoup) -> pd.Series:
+        # Get path to newest pdf
+        pdf_path = self._parse_last_pdf_link(soup)
+        # Get text from pdf
+        text = self._extract_text_from_pdf(pdf_path)
+        # Get vaccine table from text
+        df_vax = self._parse_vaccines_table_as_df(text)
+        people_vaccinated = df_vax.doses_1.sum()
+        people_fully_vaccinated = df_vax.doses_2.sum()
+        total_vaccinations = people_vaccinated + people_fully_vaccinated
+        vaccine = ", ".join(df_vax.vaccine.map(vaccines_mapping))
+        # Get date
+        regex = r"Situation Report\s+([\d\.]{10})"
+        date = re.search(regex, text).group(1)
+        date = clean_date(date, "%d.%m.%Y")
+        # Build data series
+        return pd.Series(data={
+            "total_vaccinations": total_vaccinations,
+            "people_vaccinated": people_vaccinated,
+            "people_fully_vaccinated": people_fully_vaccinated,
+            "date": date,
+            "source_url": pdf_path,
+            "vaccine": vaccine,
+            "location": self.location,
+        })
 
-def pipeline(ds: pd.Series) -> pd.Series:
-    return (
-        ds
-        .pipe(enrich_location)
-        .pipe(enrich_vaccine)
-    )
+    def _parse_last_pdf_link(self, soup):
+        links = soup.find(class_="rt-article").find_all("a")
+        for link in links:
+            if "sitrep-sl-en" in link["href"]:
+                pdf_path = "https://www.epid.gov.lk" + link["href"]
+                break
+        if not pdf_path:
+            raise ValueError("No link to PDF file was found!")
+        return pdf_path
+
+    def _extract_text_from_pdf(self, pdf_path):
+        with tempfile.NamedTemporaryFile() as tf:
+            with open(tf.name, mode="wb") as f:
+                f.write(requests.get(pdf_path).content)
+            with open(tf.name, mode="rb") as f:
+                reader = PyPDF2.PdfFileReader(f)
+                page = reader.getPage(0)
+                text = page.extractText().replace("\n", "")
+        return text
+
+    def _parse_vaccines_table_as_df(self, text):
+        # Extract doses relevant sentence
+        regex = r"COVID-19 Vaccination 1st Dose 2nd Dose (.*) District 1st Wave"    
+        vax_info = re.search(regex, text).group(1).strip()
+        # Sentence to DataFrame
+        results = re.findall(r"([a-zA-Z ]+) +(\d+) (-|\d+)", vax_info)
+        df = (
+            pd.DataFrame(results, columns=["vaccine", "doses_1", "doses_2"])
+            .replace("-", 0)
+        )
+        df = (
+            df
+            .astype({"doses_1": int, "doses_2": int})
+            .assign(vaccine=df.vaccine.str.strip())
+        )
+        return df
+
+    def to_csv(self, paths):
+        data = self.read()
+        increment(
+            paths=paths,
+            location=data["location"],
+            total_vaccinations=data["total_vaccinations"],
+            people_vaccinated=data["people_vaccinated"],
+            people_fully_vaccinated=data["people_fully_vaccinated"],
+            date=data["date"],
+            source_url=data["source_url"],
+            vaccine=data["vaccine"]
+        )
 
 
 def main(paths):
-    source = "https://www.epid.gov.lk/web/index.php?option=com_content&view=article&id=225&lang=en"
-    data = read(source).pipe(pipeline)
-    increment(
-        paths=paths,
-        location=data["location"],
-        total_vaccinations=data["total_vaccinations"],
-        people_vaccinated=data["people_vaccinated"],
-        people_fully_vaccinated=data["people_fully_vaccinated"],
-        date=data["date"],
-        source_url=data["source_url"],
-        vaccine=data["vaccine"]
-    )
+    SriLanka().to_csv(paths)
 
 
 if __name__ == "__main__":

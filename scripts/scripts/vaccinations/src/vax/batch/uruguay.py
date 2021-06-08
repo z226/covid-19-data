5aa51a16bf28e47b13d2367457f254fc885a80a6
David <3dgiordano@gmail.com>
2021-06-08 13:28:56 -0300

    By age group

--- scripts/scripts/vaccinations/src/vax/batch/uruguay.py
+++ scripts/scripts/vaccinations/src/vax/batch/uruguay.py
@@ -37,6 +37,22 @@ def main(paths):
 
     df_manufacturer.to_csv(paths.tmp_vax_out_man("Uruguay"), index=False)
 
+    # Generate Age data
+    df_age = pd.read_csv(
+        "https://raw.githubusercontent.com/3dgiordano/covid-19-uy-vacc-data/main/data/Age.csv",
+        usecols=lambda x: x == "date" or x.startswith("coverage_people_") and (
+                    x.split("_")[2].endswith("5") or x.split("_")[2] == "18")
+    ).rename(columns=lambda x: x.replace("coverage_people_", "")).replace(to_replace=r"%", value="",
+                                                                          regex=True).set_index("date")
+
+    df_age.columns = df_age.columns.str.split("_", expand=True)
+    df_age = df_age.stack(dropna=True).stack(dropna=True).rename_axis(
+        ("date", "age_group_max", "age_group_min")).reset_index()
+    df_age.rename(columns={0: "people_vaccinated_per_100"}, inplace=True)
+    df_age = df_age[["date", "age_group_min", "age_group_max", "people_vaccinated_per_100"]].sort_values(
+        ["date", "age_group_min"])
+
+    df_age.to_csv(paths.tmp_vax_out_by_age_group("Uruguay"), index=False)
 
 if __name__ == "__main__":
     main()

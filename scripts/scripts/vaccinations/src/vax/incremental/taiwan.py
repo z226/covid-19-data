1117a379cf09497d773c1b02d49fbcf7841bd492
lucas rg <lucasrg@kth.se>
2021-06-11 12:56:56 +0100

    Minor changes: extract vaccines, format check #1538

--- scripts/scripts/vaccinations/src/vax/incremental/taiwan.py
+++ scripts/scripts/vaccinations/src/vax/incremental/taiwan.py
@@ -8,6 +8,12 @@ from vax.utils.utils import get_soup
 from vax.utils.incremental import clean_count, enrich_data, increment, clean_date
 
 
+vaccines_mapping = {
+    "AstraZeneca": "Oxford/AstraZeneca",
+    "Moderna": "Moderna",
+}
+
+
 def read(source: str) -> pd.Series:
     url = f"{source}/Category/Page/9jFXNbCe-sFK9EImRRi2Og"
     soup = get_soup(url)
@@ -17,6 +23,7 @@ def read(source: str) -> pd.Series:
         "total_vaccinations": parse_people_vaccinated(df),
         "people_vaccinated": parse_people_vaccinated(df),
         "date": parse_date(soup),
+        "vaccine": parse_vaccines(df),
     })
 
 
@@ -35,6 +42,10 @@ def parse_table(url_pdf: str) -> int:
     kwargs = {"pandas_options": {"dtype": str, "header": None}}
     dfs_from_pdf = tabula.read_pdf(url_pdf, pages="all", **kwargs)
     df = dfs_from_pdf[0]
+    if df.shape[1] != 3:
+        raise ValueError(f"Table format has changed! New columns were added")
+    if df.shape[0] < 3:
+        raise ValueError(f"Table format has changed! Not enough rows!")
     # df = df.dropna(subset=[2])
     return df
 
@@ -49,6 +60,14 @@ def parse_people_vaccinated(df: pd.DataFrame) -> int:
     num = df.iloc[-1, 2]
     num = re.match(r"([0-9,]+)", num).group(1)
     return clean_count(num)
+
+
+def parse_vaccines(df: pd.DataFrame) -> str:
+    vaccines = set(df.iloc[1:-1, 0])
+    vaccines_wrong = vaccines.difference(vaccines_mapping)
+    if vaccines_wrong:
+        raise ValueError(f"Invalid vaccines: {vaccines_wrong}")
+    return ", ".join(sorted(vaccines_mapping[vax] for vax in vaccines))
 
 
 def parse_date(soup) -> str:
@@ -67,16 +86,11 @@ def enrich_location(ds: pd.Series) -> pd.Series:
     return enrich_data(ds, "location", "Taiwan")
 
 
-def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "vaccine", "Oxford/AstraZeneca, Moderna")
-
-
 def pipeline(ds: pd.Series) -> pd.Series:
     return (
         ds
         .pipe(enrich_metrics)
         .pipe(enrich_location)
-        .pipe(enrich_vaccine)
     )
 
 

d0e543a86e22ec87cc452a97cc19e49ae0f82b03
lucas rg <lucasrg@kth.se>
2021-06-14 22:31:07 +0100

    R to Python

--- scripts/scripts/testing/automations/batch/czechia.py
+++ scripts/scripts/testing/automations/batch/czechia.py
@@ -9,7 +9,7 @@ def read(source_url):
 def pipeline(df: pd.DataFrame, location: str) -> pd.DataFrame:
     # Rename
     df = df.rename(columns={
-        "Datum": "Date",
+        "datum": "Date",
         "pocet_PCR_testy": "pcr",
         "pocet_AG_testy": "antigen",
         "incidence_pozitivni": "positive",
@@ -17,22 +17,23 @@ def pipeline(df: pd.DataFrame, location: str) -> pd.DataFrame:
     # New columns
     df = df.assign(**{
         "Daily change in cumulative total": df.pcr + df.antigen,
-    })
-    df = df.assign(**{
-        "Positive rate": (
-            (df["positive"].rolling(7).sum()/df["Daily change in cumulative total"].rolling(7).sum()).round(3)
-        ),
         "Country": location,
         "Units": "tests performed",
         "Source URL": "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19",
         "Source label": "Ministry of Health",
-        "Notes": pd.NA
+        "Notes": pd.NA,
+        "Cumulative total": pd.NA,
+    })
+    df = df.assign(**{
+        "Positive rate": (
+            (df["positive"].rolling(7).sum()/df["Daily change in cumulative total"].rolling(7).sum()).round(3)
+        )
     })
     # Order
     df = df.sort_values("Date")
     # Output
     df = df[[
-        "Date", "Daily change in cumulative total", "Positive rate", "Cumulative total", "Units",
+        "Date", "Daily change in cumulative total", "Positive rate", "Cumulative total", "Country", "Units",
         "Source URL", "Source label", "Notes"
     ]]
     return df
@@ -43,7 +44,7 @@ def main():
     location = "Czechia"
     df = read(source_url).pipe(pipeline, location)
     df.to_csv(
-        f"automated_sheets_new/{location}.csv",
+        f"automated_sheets/{location}.csv",
         index=False
     )
 
---
+++ scripts/scripts/testing/automations/batch/germany.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+import tempfile
+
+import pandas as pd
+import requests
+
+
+def read_xlsx_from_url(url, **kwargs):
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux i686)"}
+    response = requests.get(url, headers=headers)
+    with tempfile.NamedTemporaryFile() as tmp:
+        with open(tmp.name, 'wb') as f:
+            f.write(response.content)
+        df = pd.read_excel(tmp.name, **kwargs)
+    df = df.dropna(how="all")
+    return df
+
+
+class Germany:
+
+    def __init__(self):
+        self.location = "Germany"
+        self.source_url = "https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Testzahlen-gesamt.xlsx?__blob=publicationFile"
+    
+    def read(self):
+        df = read_xlsx_from_url(self.source_url, sheet_name="1_Testzahlerfassung")
+        mask = df.Kalenderwoche.str.match(r"\d{1,2}/\d{4}")
+        df = df[mask]
+        return df
+    
+    def pipeline(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df.assign(**{
+            "Date": df.Kalenderwoche.apply(lambda x: datetime.strptime(x + " +1", "%W/%Y +%w").strftime("%Y-%m-%d")),
+            "Cumulative total": df["Anzahl Testungen"].cumsum(),
+            "Positive rate": (df["Positivenanteil (%)"]/100).round(3),
+            "Source URL": self.source_url,
+            "Source label": "Robert Koch Institut",
+            "Units": "tests performed",
+            "Country": self.location,
+            "Notes": pd.NA,
+        }).sort_values("Date")
+
+        df = df[[
+            "Date", "Cumulative total", "Positive rate", "Source URL", "Source label", "Units", "Country", "Notes"
+        ]]
+        return df
+
+    def to_csv(self):
+        output_path = f"automated_sheets/{self.location}.csv"
+        df = self.read().pipe(self.pipeline)
+        df.to_csv(
+            output_path,
+            index=False
+        )
+
+
+def main():
+    Germany().to_csv()
+
+
+if __name__ == "__main__":
+    main()

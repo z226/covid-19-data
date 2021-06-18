a5e67dadc6b32faf493d66cc05f7c192a5ed4f5e
lucas rg <lucasrg@kth.se>
2021-06-18 15:51:11 +0100

    fix(switzerland): Correct file path

--- scripts/scripts/vaccinations/src/vax/batch/switzerland.py
+++ scripts/scripts/vaccinations/src/vax/batch/switzerland.py
@@ -1,128 +1,136 @@
 import os
-import requests
 import tempfile
+from zipfile import ZipFile
 
 import pandas as pd
-from bs4 import BeautifulSoup
 
+from vax.utils.utils import get_soup, download_file_from_url
 
-def read_datasets_download_link(source: str) -> str:
-    page = requests.get(source).content
-    soup = BeautifulSoup(page, "html.parser")
-    for elem in soup.find_all("a", class_="footer__nav__link"):
-        if "sources-csv" in elem.get("href"):
-            return "https://www.covid19.admin.ch" + elem.get("href")
-    raise Exception("No CSV link found in footer.")
+class Switzerland:
 
+    def __init__(self):
+        self.location = "Switzerland"
+        self.source_url = "https://www.covid19.admin.ch/en/epidemiologic/vacc-doses"
 
-def read_vaccination_datasets(source: str):
-    csv_url = read_datasets_download_link(source)
-    with tempfile.TemporaryDirectory(dir=".") as temp_dir:
-        temp_file = f"{temp_dir}/data.zip"
-        os.system(f"curl {csv_url} -o {temp_file} -s > /dev/null")
-        os.system(f"unzip -d {temp_dir} {temp_file} > /dev/null")
-        doses = pd.read_csv(
-            os.path.join(temp_dir, "data/COVID19VaccDosesAdministered.csv"),
-            usecols=["geoRegion", "date", "sumTotal", "type"]
+    def read(self):
+        zip_url = self._parse_file_url()
+        df, df_manufacturer = self._parse_data(zip_url)
+        return df, df_manufacturer
+
+    def _parse_file_url(self) -> str:
+        soup = get_soup(self.source_url)
+        for elem in soup.find_all("a", class_="footer__nav__link"):
+            if "sources-csv" in elem.get("href"):
+                return "https://www.covid19.admin.ch" + elem.get("href")
+        raise Exception("No CSV link found in footer.")
+
+    def _parse_data(self, url):
+        with tempfile.TemporaryDirectory(dir=".") as temp_dir:
+            zip_path = os.path.join(temp_dir, "file.zip")
+            download_file_from_url(url, zip_path)
+            with ZipFile(zip_path, 'r') as zipObj:
+                # Extract all the contents of zip file in current directory
+                zipObj.extractall(temp_dir)
+            doses = pd.read_csv(
+                os.path.join(temp_dir, "data/COVID19VaccDosesAdministered.csv"),
+                usecols=["geoRegion", "date", "sumTotal", "type"]
+            )
+            people = pd.read_csv(
+                os.path.join(temp_dir, "data/COVID19VaccPersons.csv"),
+                usecols=["geoRegion", "date", "sumTotal", "type"]
+            )
+            manufacturer = pd.read_csv(
+                os.path.join(temp_dir, "data/COVID19AdministeredDoses_vaccine.csv"),
+                usecols=["date", "geoRegion", "vaccine", "sumTotal"]
+            )
+            return pd.concat([doses, people], ignore_index=True), manufacturer
+
+    def pipe_filter_country(self, df: pd.DataFrame, country_code: str) -> pd.DataFrame:
+        return df[df.geoRegion == country_code]
+
+    def pipe_pivot(self, df: pd.DataFrame) -> pd.DataFrame:
+        return (
+            df.pivot(index=["geoRegion", "date"], columns="type", values="sumTotal")
+            .reset_index()
+            .sort_values("date")
         )
-        people = pd.read_csv(
-            os.path.join(temp_dir, "data/COVID19FullyVaccPersons.csv"),
-            usecols=["geoRegion", "date", "sumTotal", "type"]
+
+    def pipe_rename_columns(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.rename(columns={
+            "geoRegion": "location",
+            "COVID19FullyVaccPersons": "people_fully_vaccinated",
+            "COVID19VaccDosesAdministered": "total_vaccinations",
+            "COVID19AtLeastOneDosePersons": "people_vaccinated",
+        })
+
+    def pipe_translate_country_code(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.assign(
+            location=df.location.replace({
+                "CH": "Switzerland",
+                "FL": "Liechtenstein"
+            })
         )
-        manufacturer = pd.read_csv(
-            os.path.join(temp_dir, "data/COVID19AdministeredDoses_vaccine.csv"),
-            usecols=["date", "geoRegion", "vaccine", "sumTotal"]
+
+    def pipe_source(self, df: pd.DataFrame, country_code: str) -> pd.DataFrame:
+        return df.assign(
+            source_url=f"{self.source_url}?detGeo={country_code}",
         )
-        return pd.concat([doses, people], ignore_index=True), manufacturer
 
+    def pipe_vaccine(self, df: pd.DataFrame) -> pd.DataFrame:
+        def _enrich_vaccine(date: str) -> str:
+            if date >= "2021-01-29":
+                return "Moderna, Pfizer/BioNTech"
+            return "Pfizer/BioNTech"
 
-def filter_country(df: pd.DataFrame, country_code: str) -> pd.DataFrame:
-    return df[df.geoRegion == country_code]
+        return df.assign(vaccine=df.date.astype(str).apply(_enrich_vaccine))
 
+    def pipeline(self, df: pd.DataFrame, country_code: str) -> pd.DataFrame:
+        return (
+            df
+            .pipe(self.pipe_filter_country, country_code)
+            .pipe(self.pipe_pivot)
+            .pipe(self.pipe_rename_columns)
+            .pipe(self.pipe_translate_country_code)
+            .pipe(self.pipe_source, country_code)
+            .pipe(self.pipe_vaccine)
+            [[
+                "location", "date", "vaccine", "source_url", "total_vaccinations", "people_vaccinated",
+                "people_fully_vaccinated",
+            ]]
+        )
 
-def pivot(df: pd.DataFrame) -> pd.DataFrame:
-    return (
-        df.pivot(index=["geoRegion", "date"], columns="type", values="sumTotal")
-        .reset_index()
-        .sort_values("date")
-    )
+    def pipeline_manufacturer(self, df: pd.DataFrame) -> pd.DataFrame:
+        vaccine_mapping = {
+            "pfizer_biontech": "Pfizer/BioNTech",
+            "moderna": "Moderna",
+        }
+        assert set(df["vaccine"].unique()) == set(vaccine_mapping.keys())
+        return (
+            df
+            .rename(columns={"sumTotal": "total_vaccinations"})
+            .drop(columns="geoRegion")
+            .assign(location="Switzerland")
+            .replace(vaccine_mapping)
+        )
 
+    def to_csv(self, paths):
+        vaccine_data, manufacturer_data = self.read()
 
-def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
-    return df.rename(columns={
-        "geoRegion": "location",
-        "COVID19FullyVaccPersons": "people_fully_vaccinated",
-        "COVID19VaccDosesAdministered": "total_vaccinations",
-    })
+        vaccine_data.pipe(self.pipeline, country_code="CH").to_csv(
+            paths.tmp_vax_out("Switzerland"),
+            index=False
+        )
 
+        vaccine_data.pipe(self.pipeline, country_code="FL").to_csv(
+            paths.tmp_vax_out("Liechtenstein"),
+            index=False
+        )
 
-def translate_country_code(df: pd.DataFrame) -> pd.DataFrame:
-    return df.assign(
-        location=df.location.replace({"CH": "Switzerland", "FL": "Liechtenstein"})
-    )
-
-
-def enrich(df: pd.DataFrame, country_code: str) -> pd.DataFrame:
-    return df.assign(
-        people_vaccinated=df.total_vaccinations - df.people_fully_vaccinated,
-        source_url=f"https://www.covid19.admin.ch/en/epidemiologic/vacc-doses?detGeo={country_code}",
-    )
-
-
-def enrich_vaccine(df: pd.DataFrame) -> pd.DataFrame:
-    def _enrich_vaccine(date: str) -> str:
-        if date >= "2021-01-29":
-            return "Moderna, Pfizer/BioNTech"
-        return "Pfizer/BioNTech"
-
-    return df.assign(vaccine=df.date.astype(str).apply(_enrich_vaccine))
-
-
-def pipeline(df: pd.DataFrame, country_code: str) -> pd.DataFrame:
-    return (
-        df
-        .pipe(filter_country, country_code)
-        .pipe(pivot)
-        .pipe(rename_columns)
-        .pipe(translate_country_code)
-        .pipe(enrich, country_code)
-        .pipe(enrich_vaccine)
-    )
-
-
-def manufacturer_pipeline(df: pd.DataFrame) -> pd.DataFrame:
-    vaccine_mapping = {
-        "pfizer_biontech": "Pfizer/BioNTech",
-        "moderna": "Moderna",
-    }
-    assert set(df["vaccine"].unique()) == set(vaccine_mapping.keys())
-    return (
-        df
-        .rename(columns={"sumTotal": "total_vaccinations"})
-        .drop(columns="geoRegion")
-        .assign(location="Switzerland")
-        .replace(vaccine_mapping)
-    )
+        manufacturer_data.pipe(self.pipeline_manufacturer).to_csv(
+            paths.tmp_vax_out_man("Switzerland"),
+            index=False
+        )
 
 
 def main(paths):
-    source = "https://www.covid19.admin.ch/en/epidemiologic/vacc-doses"
-    vaccine_data, manufacturer_data = read_vaccination_datasets(source)
-
-    vaccine_data.pipe(pipeline, country_code="CH").to_csv(
-        paths.tmp_vax_out("Switzerland"),
-        index=False
-    )
-
-    vaccine_data.pipe(pipeline, country_code="FL").to_csv(
-        paths.tmp_vax_out("Liechtenstein"),
-        index=False
-    )
-
-    manufacturer_data.pipe(manufacturer_pipeline).to_csv(
-        paths.tmp_vax_out_man("Switzerland"),
-        index=False
-    )
-
-if __name__ == "__main__":
-    main()
+    Switzerland().to_csv(paths)

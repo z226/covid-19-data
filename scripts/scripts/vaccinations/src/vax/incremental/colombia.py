fae82dab1bfeb408d7debf950893794fefa66ee2
lucas rg <lucasrg@kth.se>
2021-06-21 16:27:25 +0200

    fix(vax,colombia): Using home gsheet api

--- scripts/scripts/vaccinations/src/vax/incremental/colombia.py
+++ scripts/scripts/vaccinations/src/vax/incremental/colombia.py
@@ -1,85 +1,92 @@
-import os
-
 import pandas as pd
-import json 
-
-import gspread
 
 from vax.utils.incremental import enrich_data, increment, clean_count
 from vax.utils.dates import clean_date
 
 
-def read(source: str) -> pd.Series:
-    return connect_parse_data(source)
+class Colombia:
 
-def open_google_sheet(source: str):
-    vax_credentials_file = os.environ['OWID_COVID_VAX_CREDENTIALS_FILE']
-    with open(vax_credentials_file) as f:
-        data = json.load(f)
-        google_credentials_json = data['google_credentials']
+    def __init__(self, gsheets_api) -> None:
+        self.location = "Colombia"
+        self.source_url = "https://docs.google.com/spreadsheets/d/1eblBeozGn1soDGXbOIicwyEDkUqNMzzpJoAKw84TTA4"
+        self.gsheets_api = gsheets_api
 
-    gc = gspread.service_account(google_credentials_json)
+    @property
+    def sheet_id(self):
+        return self.source_url.split("/")[-1]
 
-    ssh = gc.open_by_url(source)
-    wks = ssh.get_worksheet(1)
+    def read(self) -> pd.Series:
+        ws = self.gsheets_api.get_worksheet(self.sheet_id, "Reporte diario")
+        return self._parse_data(ws)
 
-    return wks
+    def _parse_data(self, worksheet):
+        if worksheet.nrows != 44:
+            raise ValueError("Sheet format changed!")
+        total_vaccinations = self._parse_total_vaccinations(worksheet)
+        people_fully_vaccinated = self._parse_people_fully_vaccinated(worksheet)
+        if total_vaccinations is None or people_fully_vaccinated is None:
+            return None
+        return pd.Series({
+            "date": self._parse_date(worksheet),
+            "total_vaccinations": total_vaccinations,
+            "people_fully_vaccinated": people_fully_vaccinated,
+        })
 
-def connect_parse_data(source: str) -> pd.Series:
-    sheet = open_google_sheet(source)
+    def _parse_total_vaccinations(self, worksheet):
+        nrow_doses_1 = 15
+        if worksheet.at(nrow_doses_1, 9) == "Total dosis aplicadas":
+            return worksheet.at(nrow_doses_1, 10)
 
-    date = sheet.get('C44').first().strip()
-    total_vaccinations = int(sheet.get('K16').first().strip().replace(',', ''))
-    people_fully_vaccinated = int(sheet.get('K27').first().strip().replace(',', ''))
+    def _parse_people_fully_vaccinated(self, worksheet):
+        nrow_doses_1 = 26
+        if worksheet.at(nrow_doses_1, 9) == "Total dosis acumuladas segundas dosis":
+            return worksheet.at(nrow_doses_1, 10)
 
-    people_vaccinated = total_vaccinations - people_fully_vaccinated
+    def _parse_date(self, worksheet):
+        nrow_date = 43
+        if worksheet.at(nrow_date, 1) == "Fecha de corte:":
+            return clean_date(worksheet.at(nrow_date, 2), "%d/%m/%Y")
+        else:
+            raise ValueError("Date is not where it is expected be! Check worksheet")
 
-    return pd.Series({
-        "total_vaccinations": total_vaccinations,
-        "people_vaccinated": people_vaccinated,
-        "people_fully_vaccinated": people_fully_vaccinated,
-        "date": clean_date(date, "%d/%m/%Y")
-    })
+    def pipe_metrics(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "people_vaccinated", ds.total_vaccinations - ds.people_fully_vaccinated)
 
+    def pipe_location(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "location", "Colombia")
 
-def enrich_location(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "location", "Colombia")
+    def pipe_vaccine(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "vaccine", "Oxford/AstraZeneca, Pfizer/BioNTech, Sinovac")
 
+    def pipe_source(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "source_url", self.source_url)
 
-def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "vaccine", "Oxford/AstraZeneca, Pfizer/BioNTech, Sinovac")
+    def pipeline(self, ds: pd.Series) -> pd.Series:
+        return (
+            ds
+            .pipe(self.pipe_metrics)
+            .pipe(self.pipe_location)
+            .pipe(self.pipe_vaccine)
+            .pipe(self.pipe_source)
+        )
 
-
-def enrich_source(ds: pd.Series, source: str) -> pd.Series:
-    return enrich_data(ds, "source_url", source)
-
-
-def pipeline(ds: pd.Series, source: str) -> pd.Series:
-    return (
-        ds
-        .pipe(enrich_location)
-        .pipe(enrich_vaccine)
-        .pipe(enrich_source, source)
-    )
-
-
-def main(paths):
-    source = (
-        "https://docs.google.com/spreadsheets/d/1eblBeozGn1soDGXbOIicwyEDkUqNMzzpJoAKw84TTA4"
-    )
-    data = read(source).pipe(pipeline, source)
-
-    increment(
-        paths=paths,
-        location=data["location"],
-        total_vaccinations=data["total_vaccinations"],
-        people_vaccinated=data["people_vaccinated"],
-        people_fully_vaccinated=data["people_fully_vaccinated"],
-        date=data["date"],
-        source_url=data["source_url"],
-        vaccine=data["vaccine"]
-    )
+    def to_csv(self, paths):
+        data = self.read()
+        if "total_vaccinations" in data:
+            data = data.pipe(self.pipeline)
+            increment(
+                paths=paths,
+                location=data["location"],
+                total_vaccinations=data["total_vaccinations"],
+                people_vaccinated=data["people_vaccinated"],
+                people_fully_vaccinated=data["people_fully_vaccinated"],
+                date=data["date"],
+                source_url=data["source_url"],
+                vaccine=data["vaccine"]
+            )
+        else:
+            print("skipped")
 
 
-if __name__ == "__main__":
-    main()
+def main(paths, gsheets_api):
+    Colombia(gsheets_api=gsheets_api).to_csv(paths)

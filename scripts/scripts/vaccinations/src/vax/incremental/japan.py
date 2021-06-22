9b3e97b24ef810fe5e1a06a80e02da893bda84cf
Edouard Mathieu <edmat@pm.me>
2021-06-22 11:19:08 +0200

    fix(japan): new spreadsheet formats

--- scripts/scripts/vaccinations/src/vax/incremental/japan.py
+++ scripts/scripts/vaccinations/src/vax/incremental/japan.py
@@ -15,72 +15,70 @@ vaccine_mapping = {
 
 class Japan:
 
-    def __init__(self, source_url_health: str, source_url_old: str,source_url_ref: str, location: str):
+    def __init__(self, source_url_health: str, source_url_general: str, source_url_ref: str, location: str):
         self.source_url_health = source_url_health
-        self.source_url_old = source_url_old
+        self.source_url_general = source_url_general
         self.source_url_ref = source_url_ref
         self.location = location
 
     def read(self):
-        ds_health = self._parse_data(self.source_url_health)
-        ds_old = self._parse_data(self.source_url_old)
-        return self._merge_series(ds_health, ds_old)
-
-    def _parse_colnames(self, df: pd.DataFrame) -> pd.DataFrame:
-        cols_new = []
-        for col in df.columns:
-            col_new = (clean_column_name(col[0]), clean_column_name(col[1]))
-            cols_new.append(col_new)
-        df.columns = pd.MultiIndex.from_tuples(cols_new)
-        return df
-
-    def _get_data_raw(self, source: str) -> pd.DataFrame:
-        df = pd.read_excel(source, header=[2, 3]).dropna(axis=1, how="all")
-        df = self._parse_colnames(df)
-        df = df.set_index("集計日")
-        # Sanity checks
-        cond1 = df.columns.levels[0].difference(['内1回目', '内2回目', '接種回数', '曜日', '集計日'])
-        cond2 = df.columns.levels[1].difference(['', 'ファイザー社', '武田/モデルナ社'])
-        if not (cond1.empty and cond2.empty):
-            raise ValueError("Columns are not as expected. Unknown field detected, maybe due to new vaccine added!")
-        return df
+        ds_health = self._parse_data(self.source_url_health, skiprows=3)
+        ds_general = self._parse_data(self.source_url_general, skiprows=4)
+        return self._merge_series(ds_health, ds_general)
 
     def _parse_vaccines(self, df):
-        x = df.loc["合計", "内1回目"]
-        vax1 = x[x != 0].index.tolist()
-        x = df.loc["合計", "内2回目"]
-        vax2 = x[x != 0].index.tolist()
-        vaccines = set(vax1+vax2)
-        vaccines_wrong = vaccines.difference(vaccine_mapping)
-        if vaccines_wrong:
-            raise ValueError(f"New vaccine:  {vaccines_wrong}. Update `vaccine_mapping`.")
-        return ", ".join([vaccine_mapping[v] for v in vaccines])
+        vaccines = []
+        processed = []
 
-    def _parse_data(self, source: str) -> pd.Series:
-        df = self._get_data_raw(source)
+        for col in df.columns:
+            if "Unnamed" in col:
+                processed.append(col)
+                pass
+            else:
+                for japanese_name, english_name in vaccine_mapping.items():
+                    if japanese_name in col:
+                        vaccines.append(english_name)
+                        processed.append(col)
+
+        assert len(processed) == len(df.columns), f"New vaccine found! Update `vaccine_mapping`"
+        return ", ".join(list(set(vaccines)))
+
+    def _parse_data(self, source: str, skiprows: int) -> pd.Series:
+
+        df = pd.read_excel(source, usecols="A:G", skiprows=skiprows, nrows=2)
+
+        # Sanity checks
+        assert [*df.columns] == [
+            'Unnamed: 0', 'Unnamed: 1', 'Unnamed: 2', 'ファイザー社', '武田/モデルナ社',
+            'ファイザー社.1', '武田/モデルナ社.1'
+        ], "Columns are not as expected. Unknown field detected."
+
         # Parse metrics
-        total_vaccinations = df.loc["合計", "接種回数"].item()
-        people_vaccinated = df.loc["合計", "内1回目"].sum()
-        people_fully_vaccinated = df.loc["合計", "内2回目"].sum()
+        total_vaccinations = df.iloc[0, 2]
+        people_vaccinated = df.iloc[0, 3] + df.iloc[0, 4]
+        people_fully_vaccinated = df.iloc[0, 5] + df.iloc[0, 6]
+
         # Parse date
-        date = clean_date(max(dt for dt in df.index.values if isinstance(dt, datetime)))
+        date = str(df.iloc[1, 0].date())
+
         # Parse vaccines
         vaccines = self._parse_vaccines(df)
+
         return pd.Series(data={
             "total_vaccinations": total_vaccinations,
             "people_vaccinated": people_vaccinated,
             "people_fully_vaccinated": people_fully_vaccinated,
             "date": date,
-            "vaccine": vaccines
+            "vaccine": vaccines,
         })
 
-    def _merge_series(self, ds_health: pd.Series, ds_old: pd.Series) -> pd.Series:
-        vaccine = ", ".join(set(ds_health.vaccine.split(", ") + ds_old.vaccine.split(", ")))
+    def _merge_series(self, ds_health: pd.Series, ds_general: pd.Series) -> pd.Series:
+        vaccine = ", ".join(sorted(list(set(ds_health.vaccine.split(", ") + ds_general.vaccine.split(", ")))))
         return pd.Series(data={
-            "total_vaccinations": ds_health.total_vaccinations + ds_old.total_vaccinations,
-            "people_vaccinated": ds_health.people_vaccinated + ds_old.people_vaccinated,
-            "people_fully_vaccinated": ds_health.people_fully_vaccinated + ds_old.people_fully_vaccinated,
-            "date": max([ds_health.date, ds_old.date]),
+            "total_vaccinations": ds_health.total_vaccinations + ds_general.total_vaccinations,
+            "people_vaccinated": ds_health.people_vaccinated + ds_general.people_vaccinated,
+            "people_fully_vaccinated": ds_health.people_fully_vaccinated + ds_general.people_fully_vaccinated,
+            "date": max([ds_health.date, ds_general.date]),
             "vaccine": vaccine,
         })
 
@@ -115,8 +113,8 @@ class Japan:
 
 def main(paths):
     Japan(
-        source_url_health="https://www.kantei.go.jp/jp/content/IRYO-vaccination_data2.xlsx",
-        source_url_old="https://www.kantei.go.jp/jp/content/KOREI-vaccination_data2.xlsx",
+        source_url_health="https://www.kantei.go.jp/jp/content/IRYO-vaccination_data3.xlsx",
+        source_url_general="https://www.kantei.go.jp/jp/content/KOREI-vaccination_data3.xlsx",
         source_url_ref="https://www.kantei.go.jp/jp/headline/kansensho/vaccine.html",
         location="Japan",
     ).to_csv(paths)

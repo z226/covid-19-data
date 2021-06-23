ea3a6319bca3ba9de05a2b5bb0a279d89242e843
lucas rg <lucasrg@kth.se>
2021-06-23 18:56:00 +0200

    feat(vax,uk): Refactor and minor data fixes

--- scripts/scripts/vaccinations/src/vax/batch/united_kingdom.py
+++ scripts/scripts/vaccinations/src/vax/batch/united_kingdom.py
@@ -1,113 +1,129 @@
 import pandas as pd
 
 from vax.utils.pipeline import enrich_total_vaccinations
+from uk_covid19 import Cov19API
 
 
-def build_url(area: str) -> pd.DataFrame:
-    metrics = "".join(
-        f"&metric={m}"
-        for m in [
-            "cumPeopleVaccinatedFirstDoseByPublishDate",
-            "cumPeopleVaccinatedFirstDoseByVaccinationDate",
-            "cumPeopleVaccinatedSecondDoseByPublishDate",
-            "cumPeopleVaccinatedSecondDoseByVaccinationDate",
+class UnitedKingdom:
+
+    def __init__(self) -> None:
+        self.location = "United Kingdom"
+        self.source_url = "https://coronavirus.data.gov.uk/details/vaccinations"
+
+    def read(self):
+        dfs = [
+            self._read_metrics("areaType=overview"),
+            self._read_metrics("areaType=nation"),
         ]
-    )
-    return f"https://api.coronavirus.data.gov.uk/v2/data?areaType={area}&format=csv{metrics}"
+        df = pd.concat(dfs).reset_index(drop=True)
+        return df
 
-
-def read(area: str) -> pd.DataFrame:
-    uk = pd.read_csv(build_url("overview"))
-    subnational = pd.read_csv(build_url("nation"))
-    return (
-        pd.concat([uk, subnational])
-        .reset_index(drop=True)
-        .rename(columns={"areaName": "location"})
-    )
-
-
-def enrich_people_vaccinated(df: pd.DataFrame) -> pd.DataFrame:
-    return df.assign(
-        people_vaccinated=df.cumPeopleVaccinatedFirstDoseByPublishDate.fillna(
-            df.cumPeopleVaccinatedFirstDoseByVaccinationDate
+    def _read_metrics(self, filters):
+        metrics = {
+            "date": "date",
+            "location": "areaName",
+            "areaCode": "areaCode",
+            "people_vaccinated_report": "cumPeopleVaccinatedFirstDoseByPublishDate",
+            "people_vaccinated": "cumPeopleVaccinatedFirstDoseByVaccinationDate",
+            "people_fully_vaccinated_report": "cumPeopleVaccinatedCompleteByPublishDate",
+            "people_fully_vaccinated": "cumPeopleVaccinatedCompleteByVaccinationDate",
+            "total_vaccinations": "cumVaccinesGivenByPublishDate",
+            "vaccinations_age": "vaccinationsAgeDemographics",
+        }
+        api = Cov19API(
+            filters=[filters],
+            structure=metrics,
         )
-    )
+        df = api.get_dataframe()
+        return df
 
+    def _fix_metric(self, df: pd.DataFrame, metric: str) -> pd.DataFrame:
+        return df.assign(**{
+            metric: df[f"{metric}_report"].fillna(df[metric])
+        })
 
-def enrich_people_fully_vaccinated(df: pd.DataFrame) -> pd.DataFrame:
-    return df.assign(
-        people_fully_vaccinated=df.cumPeopleVaccinatedSecondDoseByPublishDate.fillna(
-            df.cumPeopleVaccinatedSecondDoseByVaccinationDate
+    def pipe_fix_metrics(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = (
+            df
+            .pipe(self._fix_metric, "people_vaccinated")
+            .pipe(self._fix_metric, "people_fully_vaccinated")
         )
-    )
+        cols = ["people_vaccinated", "people_fully_vaccinated", "total_vaccinations"]
+        df = df.sort_values(["location", "date"])
+        _tmp = df.groupby("location", as_index=False)[cols].fillna(method="ffill").fillna(0)
+        df.loc[_tmp.index, cols] = _tmp
+        df = df.assign(total_vaccinations=df[["total_vaccinations", "people_vaccinated"]].max(axis=1))
+        return df
 
+    def pipe_aggregate_first_date(self, df: pd.DataFrame) -> pd.DataFrame:
+        return (
+            df.groupby(
+                [
+                    "location",
+                    "total_vaccinations",
+                    "people_vaccinated",
+                    "people_fully_vaccinated",
+                ],
+                as_index=False,
+                dropna=False,
+            )[["date"]]
+            .min()
+            .replace(0, pd.NA)
+        )
 
-def aggregate_first_date(df: pd.DataFrame) -> pd.DataFrame:
-    return (
-        df.groupby(
-            [
-                "location",
-                "total_vaccinations",
-                "people_vaccinated",
+    def pipe_source_url(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.assign(source_url=self.source_url)
+
+    def pipe_vaccine(self, df: pd.DataFrame) -> pd.DataFrame:
+        def _enrich_vaccine(date: str) -> str:
+            if date < "2021-01-04":
+                return "Pfizer/BioNTech"
+            elif "2021-04-07" > date >= "2021-01-04":
+                return "Oxford/AstraZeneca, Pfizer/BioNTech"
+            elif date >= "2021-04-07":
+                # https://www.reuters.com/article/us-health-coronavirus-britain-moderna-idUSKBN2BU0KG
+                return "Moderna, Oxford/AstraZeneca, Pfizer/BioNTech"
+        return df.assign(vaccine=df.date.apply(_enrich_vaccine))
+
+    def pipe_exclude_data_points(self, df: pd.DataFrame) -> pd.DataFrame:
+        # The data contains an error that creates a negative change
+        df = df[(df.location != "Northern Ireland") | (df.date != "2021-02-20")]
+        df = df[(df.location != "England") | (df.date != "2021-01-09")]
+        df = df[(df.location != "Scotland") | (df.date != "2021-01-09")]
+        return df
+
+    def pipe_select_output_cols(self, df: pd.DataFrame) -> pd.DataFrame:
+        return (
+            df
+            [[
+                "location", "date", "vaccine", "source_url", "total_vaccinations", "people_vaccinated",
                 "people_fully_vaccinated",
-            ],
-            as_index=False,
-        )[["date"]]
-        .min()
-        .replace(0, pd.NA)
-    )
+            ]]
+        )
 
+    def pipeline(self, df: pd.DataFrame) -> pd.DataFrame:
+        return (
+            df
+            .pipe(self.pipe_fix_metrics)
+            .pipe(self.pipe_aggregate_first_date)
+            .pipe(self.pipe_source_url)
+            .pipe(self.pipe_vaccine)
+            .pipe(self.pipe_exclude_data_points)
+            .pipe(self.pipe_select_output_cols)
+            .sort_values(by=["location", "date"])
+        )
 
-def enrich_source_url(df: pd.DataFrame, source_url: str) -> pd.DataFrame:
-    return df.assign(source_url=source_url)
+    def _filter_location(self, df: pd.DataFrame, location: str) -> pd.DataFrame:
+        return df[df.location == location].assign(location=location)
 
-
-def enrich_vaccine(df: pd.DataFrame) -> pd.DataFrame:
-    def _enrich_vaccine(date: str) -> str:
-        if date < "2021-01-04":
-            return "Pfizer/BioNTech"
-        elif "2021-04-07" > date >= "2021-01-04":
-            return "Oxford/AstraZeneca, Pfizer/BioNTech"
-        elif date >= "2021-04-07":
-            # https://www.reuters.com/article/us-health-coronavirus-britain-moderna-idUSKBN2BU0KG
-            return "Moderna, Oxford/AstraZeneca, Pfizer/BioNTech"
-    return df.assign(vaccine=df.date.apply(_enrich_vaccine))
-
-
-def exclude_data_points(df: pd.DataFrame) -> pd.DataFrame:
-    # The data contains an error that creates a negative change
-    df = df[(df.location != "Northern Ireland") | (df.date != "2021-02-20")]
-    df = df[(df.location != "England") | (df.date != "2021-01-09")]
-    df = df[(df.location != "Scotland") | (df.date != "2021-01-09")]
-    return df
-
-
-def pipeline(df: pd.DataFrame, source_url: str) -> pd.DataFrame:
-    return (
-        df.pipe(enrich_people_vaccinated)
-        .pipe(enrich_people_fully_vaccinated)
-        .pipe(enrich_total_vaccinations)
-        .pipe(aggregate_first_date)
-        .pipe(enrich_source_url, source_url)
-        .pipe(enrich_vaccine)
-        .pipe(exclude_data_points)
-        .sort_values(by=["location", "date"])
-    )
-
-
-def filter_location(df: pd.DataFrame, location: str) -> pd.DataFrame:
-    return df[df.location == location].assign(location=location)
+    def to_csv(self, paths):
+        df = self.read().pipe(self.pipeline)
+        for location in set(df.location):
+            df.pipe(self._filter_location, location).to_csv(
+                paths.tmp_vax_out(location),
+                index=False
+            )
 
 
 def main(paths):
-    source = "https://coronavirus.data.gov.uk/details/healthcare"
-    result = read("overview").pipe(pipeline, source)
-
-    for location in set(result.location):
-        result.pipe(filter_location, location).to_csv(
-            paths.tmp_vax_out(location), index=False
-        )
-
-
-if __name__ == "__main__":
-    main()
+    UnitedKingdom().to_csv(paths)

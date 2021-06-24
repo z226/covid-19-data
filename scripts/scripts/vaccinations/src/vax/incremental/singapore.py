6c7e33a90b275fafeb3eb864db56929244083d90
lucas rg <lucasrg@kth.se>
2021-06-24 13:45:38 +0200

    refactor(vax,singapore): Object oriented

--- scripts/scripts/vaccinations/src/vax/incremental/singapore.py
+++ scripts/scripts/vaccinations/src/vax/incremental/singapore.py
@@ -5,78 +5,75 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 from vax.utils.incremental import enrich_data, increment, clean_count
+from vax.utils.utils import get_soup
 from vax.utils.dates import clean_date
 
 
-def read(source: str) -> pd.Series:
-    soup = BeautifulSoup(requests.get(source).content, "html.parser")
-    return parse_data(soup)
+class Singapore:
+    def __init__(self) -> None:
+        self.location = "Singapore"
+        self.source_url = "https://www.moh.gov.sg/covid-19"
 
+    def read(self) -> pd.Series:
+        soup = get_soup(self.source_url)
+        return self._parse_data(soup)
 
-def parse_data(soup: BeautifulSoup) -> pd.Series:
-    data = pd.Series({
-        "date": parse_date(soup),
-        "total_vaccinations": parse_metric(soup, "Total Doses Administered"),
-        "people_vaccinated": parse_metric(soup, "Received at least First Dose"),
-        "people_fully_vaccinated": parse_metric(soup, "Completed Full Vaccination Regimen"),
-    })
-    return data
+    def _parse_data(self, soup: BeautifulSoup) -> pd.Series:
+        data = pd.Series({
+            "date": self._parse_date(soup),
+            "total_vaccinations": self._parse_metric(soup, "Total Doses Administered"),
+            "people_vaccinated": self._parse_metric(soup, "Received at least First Dose"),
+            "people_fully_vaccinated": self._parse_metric(soup, "Completed Full Vaccination Regimen"),
+        })
+        return data
 
+    def _parse_date(self, soup: BeautifulSoup) -> str:
+        for h3 in soup.find_all("h3"):
+            if "Vaccination Data" in h3.text:
+                break
+        date = re.search(r"as of (\d+ \w+ \d+)", h3.text).group(1)
+        date = str(pd.to_datetime(date).date())
+        return date
 
-def parse_date(soup: BeautifulSoup) -> str:
-    for h3 in soup.find_all("h3"):
-        if "Vaccination Data" in h3.text:
-            break
-    date = re.search(r"as of (\d+ \w+ \d+)", h3.text).group(1)
-    date = str(pd.to_datetime(date).date())
-    return date
+    def _parse_metric(self, soup: BeautifulSoup, description: str) -> int:
+        value = (
+            soup.find("strong", string=description)
+            .parent.parent.parent.parent
+            .find_all("tr")[-1]
+            .text
+        )
+        return clean_count(value)
 
+    def pipe_location(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "location", "Singapore")
 
-def parse_metric(soup: BeautifulSoup, description: str) -> int:
-    value = (
-        soup.find("strong", string=description)
-        .parent.parent.parent.parent
-        .find_all("tr")[-1]
-        .text
-    )
-    return clean_count(value)
+    def pipe_vaccine(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "vaccine", "Moderna, Pfizer/BioNTech, Sinovac")
 
+    def pipe_source(self, ds: pd.Series) -> pd.Series:
+        return enrich_data(ds, "source_url", self.source_url)
 
-def enrich_location(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "location", "Singapore")
+    def pipeline(self, ds: pd.Series) -> pd.Series:
+        return (
+            ds
+            .pipe(self.pipe_location)
+            .pipe(self.pipe_source)
+            .pipe(self.pipe_vaccine)
+        )
 
-
-def enrich_vaccine(ds: pd.Series) -> pd.Series:
-    return enrich_data(ds, "vaccine", "Moderna, Pfizer/BioNTech")
-
-
-def enrich_source(ds: pd.Series, source: str) -> pd.Series:
-    return enrich_data(ds, "source_url", source)
-
-
-def pipeline(ds: pd.Series, source: str) -> pd.Series:
-    return (
-        ds
-        .pipe(enrich_location)
-        .pipe(enrich_vaccine)
-        .pipe(enrich_source, source)
-    )
+    def to_csv(self, paths):
+        data = self.read().pipe(self.pipeline)
+        increment(
+            paths=paths,
+            location=data["location"],
+            total_vaccinations=data["total_vaccinations"],
+            people_vaccinated=data["people_vaccinated"],
+            people_fully_vaccinated=data["people_fully_vaccinated"],
+            date=data["date"],
+            source_url=data["source_url"],
+            vaccine=data["vaccine"]
+        )
 
 
 def main(paths):
-    source = "https://www.moh.gov.sg/covid-19"
-    data = read(source).pipe(pipeline, source)
-    increment(
-        paths=paths,
-        location=data["location"],
-        total_vaccinations=data["total_vaccinations"],
-        people_vaccinated=data["people_vaccinated"],
-        people_fully_vaccinated=data["people_fully_vaccinated"],
-        date=data["date"],
-        source_url=data["source_url"],
-        vaccine=data["vaccine"]
-    )
-
-
-if __name__ == "__main__":
-    main()
+    Singapore().to_csv(paths)

19bb2f3af6596eddc65e9bfdf5734a7a2881fe8c
lucas rg <lucasrg@kth.se>
2021-06-24 18:11:10 +0200

    enhance(grapher): Run updates using python. If one fails, still try the rest.

---
+++ scripts/scripts/grapherupdate.py
@@ -0,0 +1,28 @@
+import global_vaccinations
+import global_testing
+import yougov
+import vax_by_manufacturer
+import vax_by_age
+from utils.slack_client import send_error
+
+
+processes = [
+  global_vaccinations,
+  global_testing,
+  yougov,
+  vax_by_manufacturer,
+  vax_by_age,
+]
+
+
+if __name__ == "__main__":
+  for process in processes:
+    try:
+      process.update_db()
+    except Exception as e:
+      send_error(
+        channel="corona-data-updates",
+        title=f'Updated Grapher dataset: {process.DATASET_NAME}',
+        message=None,
+        trace=None
+      )

455424f42d13e835c802dc19476a0cda1d735620
lucas rg <lucasrg@kth.se>
2021-06-24 18:57:33 +0200

    fix(slack): Unassigned variable

--- scripts/scripts/utils/slack_client.py
+++ scripts/scripts/utils/slack_client.py
@@ -15,7 +15,10 @@ SUCCESS_COLOR = "#01a715"
 def send_error(channel, title, message=None, trace=None):
     if not client: return None
     title = f"Error: {title}"
-    if trace: text += f"\n```{trace}```"
+    if trace:
+        if message is None:
+            message = ""
+        message += f"\n```{trace}```"
     return client.chat_postMessage(
         channel=channel,
         attachments=[
@@ -31,7 +34,10 @@ def send_error(channel, title, message=None, trace=None):
 def send_warning(channel, title, message=None, trace=None):
     if not client: return None
     title = f"Warning: {title}"
-    if trace: text += f"\n```{trace}```"
+    if trace:
+        if message is None:
+            message = ""
+        message += f"\n```{trace}```"
     return client.chat_postMessage(
         channel=channel,
         attachments=[
@@ -46,7 +52,10 @@ def send_warning(channel, title, message=None, trace=None):
 
 def send_success(channel, title, message=None, trace=None):
     if not client: return None
-    if trace: text += f"\n```{trace}```"
+    if trace:
+        if message is None:
+            message = ""
+        message += f"\n```{trace}```"
     return client.chat_postMessage(
         channel=channel,
         attachments=[

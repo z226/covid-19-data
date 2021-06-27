fbd4255508a5c00ff11aa916d7339bccd5768b70
lucas rg <lucasrg@kth.se>
2021-06-27 19:45:15 +0200

    feat(vax,japan): Major refactor + Add data files

--- scripts/scripts/vaccinations/src/vax/batch/japan.py
+++ scripts/scripts/vaccinations/src/vax/batch/japan.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from vax.utils.utils import get_soup, clean_df_columns_multiindex
 from vax.utils.dates import clean_date_series
+from vax.utils.files import export_metadata
 
 
 class Japan:
@@ -41,8 +42,8 @@ class Japan:
         return df.rename(columns={
             "日付": "date",
             "接種回数": "total_vaccinations",
-            "内１回目": "people_vaccinated",
-            "内２回目": "people_fully_vaccinated",
+            "内１回目": "first_dose",
+            "内２回目": "second_dose",
         })
 
     def pipe_1_filter_dates(self, df: pd.DataFrame) -> pd.DataFrame:
@@ -88,6 +89,21 @@ class Japan:
         )
 
     def _parse_data_2(self, source: str, header: list, date_col: str, extra_levels: list = None) -> pd.DataFrame:
+        doses_1, doses_2 = self._parse_data_2_doses(source, header, date_col, extra_levels)
+        doses_1 = doses_1.stack().reset_index().rename(columns={
+            date_col: "date",
+            "level_1": "vaccine",
+            0: "first_dose"
+        })
+        doses_2 = doses_2.stack().reset_index().rename(columns={
+            date_col: "date",
+            "level_1": "vaccine",
+            0: "second_dose"
+        })
+        df = doses_1.merge(doses_2, on=["date", "vaccine"])
+        return df
+
+    def _parse_data_2_doses(self, source: str, header: list, date_col: str, extra_levels: list = None) -> pd.DataFrame:
         # Read general
         df = pd.read_excel(source, header=header)
         df = df.dropna(axis=1, how='all')
@@ -100,15 +116,7 @@ class Japan:
             extra_levels = []
         doses_1 = df.loc[:, tuple(extra_levels + ["内1回目"])]
         doses_2 = df.loc[:, tuple(extra_levels + ["内2回目"])]
-        return (
-            pd.DataFrame({
-                "people_vaccinated": doses_1.sum(axis=1),
-                "people_fully_vaccinated": doses_2.sum(axis=1),
-                "vaccine": self._parse_data_2_vaccines(doses_1, doses_2, date_col),
-            })
-            .reset_index()
-            .rename(columns={date_col: "date"})
-        )
+        return doses_1, doses_2
 
     def _parse_data_2_vaccines(self, doses_1, doses_2, date_col) -> pd.DataFrame:
         if not all(doses_1.columns == doses_2.columns):
@@ -123,16 +131,21 @@ class Japan:
     def pipe_2_aggregate(self, df: pd.DataFrame) -> pd.DataFrame:
         return (
             df
-            .groupby("date", as_index=False)
+            .groupby(["date", "vaccine"], as_index=False)
             .agg({
-                "people_vaccinated": "sum",
-                "people_fully_vaccinated": "sum",
-                "vaccine": lambda x: ", ".join(sorted(set(self.vaccine_mapping[xxx] for xx in list(x) for xxx in xx)))
+                "first_dose": "sum",
+                "second_dose": "sum",
             })
         )
 
+    def pipe_2_vaccine_rename(self, df: pd.DataFrame) -> pd.DataFrame:
+        vaccines_wrong = set(df.vaccine).difference(self.vaccine_mapping)
+        if vaccines_wrong:
+            raise ValueError(f"Unknown vacine(s): {vaccines_wrong}")
+        return df.assign(vaccine=df.vaccine.replace(self.vaccine_mapping))
+
     def pipe_2_metrics(self, df: pd.DataFrame) -> pd.DataFrame:
-        return df.assign(total_vaccinations=df.people_vaccinated+df.people_fully_vaccinated)
+        return df.assign(total_vaccinations=df.first_dose+df.second_dose)
 
     def pipe_2_source(self, df: pd.DataFrame) -> pd.DataFrame:
         return df.assign(source_url=self.source_url_2_ref)
@@ -144,6 +157,7 @@ class Japan:
         return (
             df
             .pipe(self.pipe_2_aggregate)
+            .pipe(self.pipe_2_vaccine_rename)
             .pipe(self.pipe_2_metrics)
             .pipe(self.pipe_2_source)
             .pipe(self.pipe_2_date)
@@ -153,26 +167,76 @@ class Japan:
         return df.assign(location=self.location)
 
     def pipe_cumsum(self, df: pd.DataFrame) -> pd.DataFrame:
-        df = df.sort_values("date")
-        column_metrics = ["total_vaccinations", "people_vaccinated", "people_fully_vaccinated"]
-        df.loc[:, column_metrics] = df[column_metrics].cumsum()
+        df = df.sort_values(["date"])
+        column_metrics = ["total_vaccinations", "first_dose", "second_dose"]
+        df.loc[:, column_metrics] = df.groupby("vaccine")[column_metrics].cumsum()
         return df
 
-    def pipeline(self, df: pd.DataFrame) -> pd.DataFrame:
+    def base_pipeline(self, df: pd.DataFrame) -> pd.DataFrame:
         return (
             df
             .pipe(self.pipe_location)
             .pipe(self.pipe_cumsum)
             [[
-                "location", "date", "vaccine", "source_url", "total_vaccinations", "people_vaccinated",
-                "people_fully_vaccinated",
+                "location", "date", "vaccine", "source_url", "total_vaccinations", "first_dose",
+                "second_dose",
             ]])
 
+    def pipeline_manufacturer(self, df: pd.DataFrame) -> pd.DataFrame:
+        return (
+            df
+            [[
+                "location", "date", "vaccine", "total_vaccinations",
+            ]]
+        )
+
+    def pipe_metrics(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.rename(columns={
+            "first_dose": "people_vaccinated",
+            "second_dose": "people_fully_vaccinated"
+        })
+
+    def pipe_aggregate(self, df: pd.DataFrame) -> pd.DataFrame:
+        return (
+            df
+            .groupby(["date", "location", "source_url"], as_index=False).agg({
+                "total_vaccinations": sum,
+                "people_vaccinated": sum,
+                "people_fully_vaccinated": sum,
+                "vaccine": lambda x: ", ".join(sorted(set(x)))
+            })
+        )
+
+    def pipeline(self, df: pd.DataFrame) -> pd.DataFrame:
+        return (
+            df
+            .pipe(self.pipe_metrics)
+            .pipe(self.pipe_aggregate)
+            [[
+                "location", "date", "vaccine", "source_url", "total_vaccinations", "people_vaccinated",
+                "people_fully_vaccinated",
+            ]]
+        )
+
     def export(self, paths):
-        df = self.read().pipe(self.pipeline)
-        df.to_csv(paths.tmp_vax_out(self.location), index=False, float_format='%.f')
+        df = self.read().pipe(self.base_pipeline)
+        # Manufacturer
+        df.pipe(self.pipeline_manufacturer).to_csv(
+            paths.tmp_vax_out_man(self.location),
+            index=False
+        )
+        export_metadata(
+            df,
+            "Prime Minister of Japan and Hist Cabinet",
+            self.source_url_2_ref,
+            paths.tmp_vax_metadata_man
+        )
+        # Main data
+        df.pipe(self.pipeline).to_csv(
+            paths.tmp_vax_out(self.location),
+            index=False
+        )
 
 
 def main(paths):
     Japan().export(paths)
-

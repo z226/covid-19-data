9b44916924dcaeac0cb64c377dd69281fb201097
lucas rg <lucasrg@kth.se>
2021-07-01 00:20:06 +0200

    refactor(vax,malaysia): object oriented

--- scripts/scripts/vaccinations/src/vax/batch/malaysia.py
+++ scripts/scripts/vaccinations/src/vax/batch/malaysia.py
@@ -1,36 +1,44 @@
 import pandas as pd
 
-def read(source: str) -> pd.DataFrame:
-    return pd.read_csv(source, usecols=["date", "dose1_cumul", "dose2_cumul", "total_cumul"])
 
-def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
-    return df.rename(columns={"dose1_cumul": "people_vaccinated", "dose2_cumul": "people_fully_vaccinated", "total_cumul": "total_vaccinations"})
+class Malaysia:
+    def __init__(self) -> None:
+        self.location = "Malaysia"
+        self.source_url = "https://raw.githubusercontent.com/CITF-Malaysia/citf-public/main/vax_malaysia.csv"
+        self.source_url_ref = "https://github.com/CITF-Malaysia/citf-public"
 
-def enrich_vaccine(date: str) -> str:
-    if date >= "2021-05-05":
-      return "Oxford/AstraZeneca, Pfizer/BioNTech, Sinovac"
-    if date >= "2021-03-17":
-      return "Pfizer/BioNTech, Sinovac"
-    return "Pfizer/BioNTech"
+    def read(self) -> pd.DataFrame:
+        return pd.read_csv(self.source_url, usecols=["date", "dose1_cumul", "dose2_cumul", "total_cumul"])
 
-def enrich_metadata(df: pd.DataFrame) -> pd.DataFrame:
-    return df.assign(
-        location="Malaysia",
-        source_url="https://github.com/CITF-Malaysia/citf-public",
-    )
+    def pipe_rename_columns(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.rename(columns={"dose1_cumul": "people_vaccinated", "dose2_cumul": "people_fully_vaccinated", "total_cumul": "total_vaccinations"})
 
-def pipeline(df: pd.DataFrame) -> pd.DataFrame:
-    return (
-        df
-        .pipe(rename_columns)
-        .assign(vaccine=df.date.astype(str).apply(enrich_vaccine))
-        .pipe(enrich_metadata)
-    )
+    def pipe_vaccine(self, df: pd.DataFrame) -> str:
+        def _enrich_vaccine(date):
+            if date >= "2021-05-05":
+                return "Oxford/AstraZeneca, Pfizer/BioNTech, Sinovac"
+            if date >= "2021-03-17":
+                return "Pfizer/BioNTech, Sinovac"
+            return "Pfizer/BioNTech"
+        return df.assign(vaccine=df.date.astype(str).apply(_enrich_vaccine))
 
+    def pipe_metadata(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.assign(
+            location=self.location,
+            source_url=self.source_url_ref,
+        )
+
+    def pipeline(self, df: pd.DataFrame) -> pd.DataFrame:
+        return (
+            df
+            .pipe(self.pipe_rename_columns)
+            .pipe(self.pipe_vaccine)
+            .pipe(self.pipe_metadata)
+        )
+
+    def export(self, paths):
+        df = self.read().pipe(self.pipeline)
+        df.to_csv(paths.tmp_vax_out(self.location), index=False)
 
 def main(paths):
-    source = "https://raw.githubusercontent.com/CITF-Malaysia/citf-public/main/vax_malaysia.csv"
-    read(source).pipe(pipeline).to_csv(paths.tmp_vax_out("Malaysia"), index=False)
-
-if __name__ == "__main__":
-    main()
+    Malaysia().export(paths)

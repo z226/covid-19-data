e469f108db8671b8c7bc99ddb6511c0ce1d0131f
lucas rg <lucasrg@kth.se>
2021-07-01 02:06:02 +0200

    fix(grapher): Fix dataset name

--- scripts/src/cowidev/grapher/db/procs/variants.py
+++ scripts/src/cowidev/grapher/db/procs/variants.py
@@ -4,7 +4,7 @@ from cowidev.grapher.db.base import GrapherBaseUpdater
 class GrapherVariantsUpdater(GrapherBaseUpdater):
     def __init__(self) -> None:
         super().__init__(
-            dataset_name='COVID-19 - Variants.csv',
+            dataset_name='COVID-19 - Variants',
             source_name=(
                 f"CoVariants: SARS-CoV-2 Mutations and Variants of Interest – Last updated {self.time_str} "
                 f"(London time)"

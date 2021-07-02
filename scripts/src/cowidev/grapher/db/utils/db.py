a272f05bb63ef86aa0aa8b50e8a4534057351fae
lucas rg <lucasrg@kth.se>
2021-07-02 02:23:12 +0200

    refactor(db): variable to property

--- scripts/src/cowidev/grapher/db/utils/db.py
+++ scripts/src/cowidev/grapher/db/utils/db.py
@@ -2,13 +2,18 @@ import os
 import pymysql
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
-# Connect to the database
-connection = pymysql.connect(db=os.getenv('DB_NAME'),
-                             host=os.getenv('DB_HOST'),
-                             port=int(os.getenv('DB_PORT')),
-                             user=os.getenv('DB_USER'),
-                             password=os.getenv('DB_PASS'),
-                             charset='utf8mb4',
-                             autocommit=False) # requires .commit(), so everything is implicitly a transaction
+@property
+def connection():
+    # Connect to the database
+    pymysql.connect(
+        db=os.getenv('DB_NAME'),
+        host=os.getenv('DB_HOST'),
+        port=int(os.getenv('DB_PORT')),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASS'),
+        charset='utf8mb4',
+        autocommit=False
+    ) # requires .commit(), so everything is implicitly a transaction

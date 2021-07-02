76c12fadcfbb468e89e40890039110825d4ff5fb
lucas rg <lucasrg@kth.se>
2021-07-02 02:26:21 +0200

    refactor(cowidev): raise error if no env found

--- scripts/src/cowidev/utils/utils.py
+++ scripts/src/cowidev/utils/utils.py
@@ -7,9 +7,10 @@ from datetime import datetime, timedelta
 def get_project_dir(err: bool = False):
     project_dir = os.environ.get("OWID_COVID_PROJECT_DIR")
     if err and project_dir is None:
-        raise ValueError(
-            "Please have ${OWID_COVID_PROJECT_DIR}."
-        )
+        pass
+    raise ValueError(
+        "Please have ${OWID_COVID_PROJECT_DIR}."
+    )
     return project_dir
 
 

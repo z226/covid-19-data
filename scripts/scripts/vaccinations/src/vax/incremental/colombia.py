c960d95a062320d36dd64946d46098bb09bac56e
Edouard Mathieu <edmat@pm.me>
2021-07-14 12:33:45 +0200

    fix(vax,colombia): change parsing logic

--- scripts/scripts/vaccinations/src/vax/incremental/colombia.py
+++ scripts/scripts/vaccinations/src/vax/incremental/colombia.py
@@ -1,3 +1,5 @@
+import re
+
 import pandas as pd
 
 from vax.utils.incremental import enrich_data, increment, clean_count
@@ -21,49 +23,26 @@ class Colombia:
         return df
 
     def _parse_data(self, worksheet):
-        if worksheet.nrows != 45:
-            raise ValueError("Sheet format changed!")
-        total_vaccinations = self._parse_total_vaccinations(worksheet)
-        people_fully_vaccinated = self._parse_people_fully_vaccinated(worksheet)
-        unique_doses = self._parse_unique_doses(worksheet)
+        
+        for row in worksheet.values():
+            for value in row:
+                if "Total dosis aplicadas al " in str(value):
+                    total_vaccinations = row[-1]
+                    date_raw = re.search(r"[\d-]{10}$", value).group(0)
+                    date_str = clean_date(date_raw, "%d-%m-%Y")
+                elif value == "Esquemas completos segundas + únicas dosis":
+                    people_fully_vaccinated = row[-1]
+                elif value == "Total únicas dosis acumuladas":
+                    unique_doses = row[-1]
+
         if total_vaccinations is None or people_fully_vaccinated is None:
-            return None
+            raise ValueError("Date is not where it is expected be! Check worksheet")
         return pd.Series({
-            "date": self._parse_date(worksheet),
+            "date": date_str,
             "total_vaccinations": total_vaccinations,
             "people_fully_vaccinated": people_fully_vaccinated,
             "people_vaccinated": total_vaccinations - people_fully_vaccinated + unique_doses,
         })
-
-    def _parse_total_vaccinations(self, worksheet):
-        nrow_total_doses = 23
-        if "Total dosis aplicadas al" in worksheet.at(nrow_total_doses, 13):
-            return worksheet.at(nrow_total_doses, 14)
-        else:
-            raise ValueError("Sheet format changed!")
-
-    def _parse_people_fully_vaccinated(self, worksheet):
-        nrow_fully_vaccinated = 40
-        if worksheet.at(nrow_fully_vaccinated, 13) == "Esquemas completos con segundas dosis y dosis única":
-            return worksheet.at(nrow_fully_vaccinated, 14)
-        else:
-            raise ValueError("Sheet format changed!")
-
-    def _parse_unique_doses(self, worksheet):
-        nrow_doses_unique = 38
-        if worksheet.at(nrow_doses_unique, 13) == "Total únicas dosis acumuladas":
-            return worksheet.at(nrow_doses_unique, 14)
-        else:
-            raise ValueError("Sheet format changed!")
-
-    def _parse_date(self, worksheet):
-        nrow_date = 44
-        if worksheet.at(nrow_date, 1) == "Fecha de corte:":
-            date_raw = worksheet.at(nrow_date, 2)
-            date_str = clean_date(date_raw, "%d/%m/%Y")
-            return date_str
-        else:
-            raise ValueError("Date is not where it is expected be! Check worksheet")
 
     def pipe_location(self, ds: pd.Series) -> pd.Series:
         return enrich_data(ds, "location", "Colombia")

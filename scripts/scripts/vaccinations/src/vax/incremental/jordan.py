007dbe76f586c28e8510b4be4d40bfe675af5af2
Edouard Mathieu <edmat@pm.me>
2021-07-15 10:44:06 +0200

    fix(vax,jordan): increase wait time

--- scripts/scripts/vaccinations/src/vax/incremental/jordan.py
+++ scripts/scripts/vaccinations/src/vax/incremental/jordan.py
@@ -12,7 +12,6 @@ from vax.utils.dates import localdate
 
 
 class Jordan:
-
     def __init__(self):
         self.location = "Jordan"
         self.source_url = "https://corona.moh.gov.jo/ar"
@@ -28,52 +27,60 @@ class Jordan:
             # Main page
             driver.get(self.source_url)
             # Get report page from within iframe
-            source = driver.find_element_by_xpath("/html/body/section[2]/iframe").get_attribute("src")
+            source = driver.find_element_by_xpath(
+                "/html/body/section[2]/iframe"
+            ).get_attribute("src")
 
             driver.get(source)
 
-            data_blocks = (
-                WebDriverWait(driver, 20)
-                .until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "card")))
+            data_blocks = WebDriverWait(driver, 30).until(
+                EC.visibility_of_all_elements_located((By.CLASS_NAME, "card"))
             )
             for block in data_blocks:
                 block_title = block.get_attribute("aria-label")
                 if "first dose" in block_title:
-                    people_vaccinated = re.search(r"first dose +(\d+)\.", block_title).group(1)
+                    people_vaccinated = re.search(
+                        r"first dose +(\d+)\.", block_title
+                    ).group(1)
                 elif "sec dose" in block_title:
-                    people_fully_vaccinated = re.search(r"sec dose +(\d+)\.", block_title).group(1)
+                    people_fully_vaccinated = re.search(
+                        r"sec dose +(\d+)\.", block_title
+                    ).group(1)
 
-            people_vaccinated=clean_count(people_vaccinated)
-            people_fully_vaccinated=clean_count(people_fully_vaccinated)
+            people_vaccinated = clean_count(people_vaccinated)
+            people_fully_vaccinated = clean_count(people_fully_vaccinated)
 
-        return pd.Series({
-            "people_vaccinated": people_vaccinated,
-            "people_fully_vaccinated": people_fully_vaccinated,
-        })
+        return pd.Series(
+            {
+                "people_vaccinated": people_vaccinated,
+                "people_fully_vaccinated": people_fully_vaccinated,
+            }
+        )
 
     def pipe_vaccinations(self, ds: pd.Series) -> pd.Series:
         total_vaccinations = ds.people_vaccinated + ds.people_fully_vaccinated
-        return enrich_data(ds, 'total_vaccinations', total_vaccinations)
+        return enrich_data(ds, "total_vaccinations", total_vaccinations)
 
     def pipe_date(self, ds: pd.Series) -> pd.Series:
         date = localdate("Asia/Amman")
-        return enrich_data(ds, 'date', date)
+        return enrich_data(ds, "date", date)
 
     def pipe_location(self, ds: pd.Series) -> pd.Series:
-        return enrich_data(ds, 'location', self.location)
+        return enrich_data(ds, "location", self.location)
 
     def pipe_vaccine(self, ds: pd.Series) -> pd.Series:
         # Johnson&Johnson authorized, no doses arrived or given yet
-        vaccines_used = "Pfizer/BioNTech, Sinopharm/Beijing, Sputnik V, Oxford/AstraZeneca"
-        return enrich_data(ds, 'vaccine', vaccines_used) 
+        vaccines_used = (
+            "Pfizer/BioNTech, Sinopharm/Beijing, Sputnik V, Oxford/AstraZeneca"
+        )
+        return enrich_data(ds, "vaccine", vaccines_used)
 
     def pipe_source(self, ds: pd.Series) -> pd.Series:
-        return enrich_data(ds, 'source_url', self.source_url)
+        return enrich_data(ds, "source_url", self.source_url)
 
     def pipeline(self, ds: pd.Series) -> pd.Series:
         return (
-            ds
-            .pipe(self.pipe_vaccinations)
+            ds.pipe(self.pipe_vaccinations)
             .pipe(self.pipe_date)
             .pipe(self.pipe_location)
             .pipe(self.pipe_vaccine)
@@ -84,13 +91,13 @@ class Jordan:
         data = self.read().pipe(self.pipeline)
         increment(
             paths=paths,
-            location=data['location'],
-            total_vaccinations=data['total_vaccinations'],
-            people_vaccinated=data['people_vaccinated'],
-            people_fully_vaccinated=data['people_fully_vaccinated'],
-            date=data['date'],
-            source_url=data['source_url'],
-            vaccine=data['vaccine']
+            location=data["location"],
+            total_vaccinations=data["total_vaccinations"],
+            people_vaccinated=data["people_vaccinated"],
+            people_fully_vaccinated=data["people_fully_vaccinated"],
+            date=data["date"],
+            source_url=data["source_url"],
+            vaccine=data["vaccine"],
         )
 
 

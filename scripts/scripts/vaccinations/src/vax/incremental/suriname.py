bee235bc2180342b3b25a487e629e96fed9dfa7f
Edouard Mathieu <edmat@pm.me>
2021-07-22 16:00:55 +0200

    fix(vax,suriname): new dashboard

--- scripts/scripts/vaccinations/src/vax/incremental/suriname.py
+++ scripts/scripts/vaccinations/src/vax/incremental/suriname.py
@@ -1,25 +1,35 @@
-import requests
-from bs4 import BeautifulSoup
+import time
+
 import pandas as pd
 
-from vax.utils.incremental import enrich_data, increment
+from vax.utils.incremental import enrich_data, increment, clean_count
 from vax.utils.dates import localdate
+from vax.utils.utils import get_driver
 
 
 def read(source: str) -> pd.Series:
-    soup = BeautifulSoup(requests.get(source).content, "html.parser")
-    return parse_data(soup)
 
+    with get_driver() as driver:
+        driver.get(source)
+        time.sleep(10)
 
-def parse_data(soup: BeautifulSoup) -> pd.Series:
+        for block in driver.find_elements_by_class_name("kpimetric"):
+            if "1ste dosis" in block.text and "%" not in block.text:
+                people_partly_vaccinated = clean_count(
+                    block.find_element_by_class_name("valueLabel").text
+                )
+            elif "2de dosis" in block.text and "%" not in block.text:
+                people_fully_vaccinated = clean_count(
+                    block.find_element_by_class_name("valueLabel").text
+                )
 
-    numbers = soup.find_all(class_="elementor-counter-number")
+    people_vaccinated = people_partly_vaccinated + people_fully_vaccinated
 
     return pd.Series(
         data={
-            "total_vaccinations": int(numbers[0]["data-to-value"]),
-            "people_vaccinated": int(numbers[1]["data-to-value"]),
-            "people_fully_vaccinated": int(numbers[2]["data-to-value"]),
+            "total_vaccinations": people_vaccinated + people_fully_vaccinated,
+            "people_vaccinated": people_vaccinated,
+            "people_fully_vaccinated": people_fully_vaccinated,
             "date": set_date(),
         }
     )
@@ -37,17 +47,17 @@ def enrich_vaccine(ds: pd.Series) -> pd.Series:
     return enrich_data(ds, "vaccine", "Oxford/AstraZeneca")
 
 
-def enrich_source(ds: pd.Series, source: str) -> pd.Series:
-    return enrich_data(ds, "source_url", source)
+def enrich_source(ds: pd.Series) -> pd.Series:
+    return enrich_data(ds, "source_url", "https://laatjevaccineren.sr/")
 
 
-def pipeline(ds: pd.Series, source: str) -> pd.Series:
-    return ds.pipe(enrich_location).pipe(enrich_vaccine).pipe(enrich_source, source)
+def pipeline(ds: pd.Series) -> pd.Series:
+    return ds.pipe(enrich_location).pipe(enrich_vaccine).pipe(enrich_source)
 
 
 def main(paths):
-    source = "https://laatjevaccineren.sr/"
-    data = read(source).pipe(pipeline, source)
+    source = "https://datastudio.google.com/embed/u/0/reporting/1a7548f9-83d0-4516-8fe6-cacec6a293c4/page/igSUC"
+    data = read(source).pipe(pipeline)
     increment(
         paths=paths,
         location=data["location"],

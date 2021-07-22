0ce73662091ed61b47b6786148ce001fd865167b
Edouard Mathieu <edmat@pm.me>
2021-07-22 22:55:27 +0200

    style(tests,chile): python linting

--- scripts/scripts/testing/automations/batch/chile.py
+++ scripts/scripts/testing/automations/batch/chile.py
@@ -1,24 +1,32 @@
 import pandas as pd
 
+
 def main():
-    pcr = pd.read_csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto49/Positividad_Diaria_Media_T.csv")
-    ag = pd.read_csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto49/Positividad_Diaria_Media_Ag_T.csv")
+    pcr = pd.read_csv(
+        "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto49/Positividad_Diaria_Media_T.csv"
+    )
+    ag = pd.read_csv(
+        "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto49/Positividad_Diaria_Media_Ag_T.csv"
+    )
 
-    pcr = pcr[['Fecha', 'pcr']]
-    ag = ag[['Fecha', 'Ag']]
+    pcr = pcr[["Fecha", "pcr"]]
+    ag = ag[["Fecha", "Ag"]]
 
-    df = pd.merge(ag, pcr, on = 'Fecha', how = 'outer').fillna(0).sort_values("Fecha")
-    df['Daily change in cumulative total'] = df['Ag']+df['pcr']
+    df = pd.merge(ag, pcr, on="Fecha", how="outer").fillna(0).sort_values("Fecha")
+    df["Daily change in cumulative total"] = df["Ag"] + df["pcr"]
     df = df.rename(columns={"Fecha": "Date"})
-    df = df.drop(['Ag','pcr'], axis=1)
+    df = df.drop(["Ag", "pcr"], axis=1)
 
-    df['Country'] = "Chile"
-    df['Source URL'] = "https://github.com/MinCiencia/Datos-COVID19/tree/master/output/producto49"
-    df['Source label'] = 'Chile Ministry of Health'
-    df['Units'] = 'tests performed'
-    df['Notes'] = pd.NA
+    df["Country"] = "Chile"
+    df[
+        "Source URL"
+    ] = "https://github.com/MinCiencia/Datos-COVID19/tree/master/output/producto49"
+    df["Source label"] = "Chile Ministry of Health"
+    df["Units"] = "tests performed"
+    df["Notes"] = pd.NA
 
     df.to_csv("automated_sheets/Chile.csv", index=False)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()

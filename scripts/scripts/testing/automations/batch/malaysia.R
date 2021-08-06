dd7c34cbc65cea86fb5ce7aa241f1f71e6293271
Cam Appel <cameron@ourworldindata.org>
2021-08-06 14:48:03 -0400

    Update and rename Malaysia.R to malaysia.R

---
+++ scripts/scripts/testing/automations/batch/malaysia.R
@@ -0,0 +1,16 @@
+df <- fread("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/tests_malaysia.csv") %>%
+  mutate(`Daily change in cumulative total` = .[[2]] + .[[3]]) %>%
+  subset(., select=-c(`pcr`, `rtk-ag`)) %>%
+  mutate(`Cumulative total`=cumsum(`Daily change in cumulative total`))
+
+setnames(df, "date", "Date")
+
+df$Date <- as.Date(df$Date)
+
+df[, Country := "Malaysia"]
+df[, `Source URL` := "https://github.com/MoH-Malaysia/covid19-public"]
+df[, `Source label` := "Malaysia Ministry of Health"]
+df[, Notes := "Made available by the Malaysia Ministry of Health on GitHub"]
+
+setorder(df, -Date)
+fwrite(df, "automated_sheets/Malaysia.csv")

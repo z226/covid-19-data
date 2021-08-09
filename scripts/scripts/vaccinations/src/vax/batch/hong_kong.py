d1802cfbd615935ff0df3035757d1585c2e02e85
Edouard Mathieu <edmat@pm.me>
2021-08-09 09:26:56 +0200

    feat(vax,hk): collect manufacturer data

--- scripts/scripts/vaccinations/src/vax/batch/hong_kong.py
+++ scripts/scripts/vaccinations/src/vax/batch/hong_kong.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+from vax.utils.files import export_metadata
 
 
 def read(source: str) -> pd.DataFrame:
@@ -10,15 +11,12 @@ def read(source: str) -> pd.DataFrame:
                 "date": d["date"],
                 "people_vaccinated": d["firstDose"]["cumulative"]["total"],
                 "people_fully_vaccinated": d["secondDose"]["cumulative"]["total"],
+                "total_vaccinations": d["totalDose"]["cumulative"]["total"],
+                "total_pfizer": d["totalDose"]["cumulative"]["biontech"],
+                "total_sinovac": d["totalDose"]["cumulative"]["sinovac"],
             }
             for d in data
         ]
-    )
-
-
-def enrich_vaccinations(df: pd.DataFrame) -> pd.DataFrame:
-    return df.assign(
-        total_vaccinations=df.people_vaccinated + df.people_fully_vaccinated
     )
 
 
@@ -38,14 +36,35 @@ def enrich_metadata(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def pipeline(df: pd.DataFrame) -> pd.DataFrame:
-    return df.pipe(enrich_vaccinations).pipe(enrich_vaccine).pipe(enrich_metadata)
+    return df.pipe(enrich_vaccine).pipe(enrich_metadata)
+
+
+def manufacturer_pipeline(df: pd.DataFrame) -> pd.DataFrame:
+    return (
+        df[["location", "date", "total_pfizer", "total_sinovac"]]
+        .rename(columns={"total_pfizer": "Pfizer/BioNTech", "total_sinovac": "Sinovac"})
+        .set_index(["location", "date"])
+        .stack()
+        .reset_index()
+        .rename(columns={"level_2": "vaccine", 0: "total_vaccinations"})
+    )
 
 
 def main(paths):
     source = "https://static.data.gov.hk/covid-vaccine/bar_vaccination_date.json"
-    destination = paths.tmp_vax_out("Hong Kong")
+    data = read(source).pipe(pipeline)
 
-    read(source).pipe(pipeline).to_csv(destination, index=False)
+    destination = paths.tmp_vax_out("Hong Kong")
+    data.drop(columns=["total_pfizer", "total_sinovac"]).to_csv(
+        destination, index=False
+    )
+
+    destination = paths.tmp_vax_out_man("Hong Kong")
+    manufacturer = data.pipe(manufacturer_pipeline)
+    manufacturer.to_csv(destination, index=False)
+    export_metadata(
+        manufacturer, "Government of Hong Kong", source, paths.tmp_vax_metadata_man
+    )
 
 
 if __name__ == "__main__":

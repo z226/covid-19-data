19f5cabc4c2e1a8bf8796aadd10e380ab96c3752
lucas rg <lucasrg@kth.se>
2021-08-16 13:42:39 +0200

    feat(vax,india): combine mohfw and cowin source

--- scripts/src/cowidev/vax/incremental/india.py
+++ scripts/src/cowidev/vax/incremental/india.py
@@ -2,23 +2,54 @@ import requests
 import pandas as pd
 
 from cowidev.vax.utils.incremental import enrich_data, increment
-from cowidev.vax.utils.dates import localdatenow, clean_date
+from cowidev.vax.utils.dates import localdate
 
 
 class India:
     def __init__(self) -> None:
         self.location = "India"
-        self.source_url = "https://www.mygov.in/sites/default/files/covid/vaccine/vaccine_counts_today.json"
-        # alt: f"https://api.cowin.gov.in/api/v1/reports/v2/getPublicReports?state_id=&district_id=&date={date_str}"
-        self.source_url_ref = "https://www.mohfw.gov.in/"
+        self.source_name = "cowin"  # mohfw, cowin
+        self.source_url = {
+            "mohfw": "https://www.mygov.in/sites/default/files/covid/vaccine/vaccine_counts_today.json",
+            "cowin": (
+                f"https://api.cowin.gov.in/api/v1/reports/v2/getPublicReports?state_id=&district_id=&date="
+                f"{self.date_str}"
+            ),
+        }
+        self.source_url_ref = {
+            "mohfw": "https://www.mohfw.gov.in/",
+            "cowin": "https://dashboard.cowin.gov.in/",
+        }
 
-    def read(self) -> pd.Series:
-        data = requests.get(self.source_url).json()
+    def read(self):
+        data = requests.get(self.source_url[self.source_name]).json()
+        if self.source_name == "mohfw":
+            return self.read_mohfw(data)
+        elif self.source_name == "cowin":
+            return self.read_cowin(data)
+        raise ValueError(f"Not valid class attribute `source_name`: {self.source_name}")
 
-        people_vaccinated = data["india_dose1"]
-        people_fully_vaccinated = data["india_dose2"]
-        total_vaccinations = data["india_total_doses"]
-        date = data["day"]
+    def read_cowin(self, json_data) -> pd.Series:
+        data = json_data["topBlock"]["vaccination"]
+
+        people_vaccinated = data["tot_dose_1"]
+        people_fully_vaccinated = data["tot_dose_2"]
+        total_vaccinations = data["total"]
+
+        return pd.Series(
+            {
+                "date": self.date_str,
+                "people_vaccinated": people_vaccinated,
+                "people_fully_vaccinated": people_fully_vaccinated,
+                "total_vaccinations": total_vaccinations,
+            }
+        )
+
+    def read_mohfw(self, json_data) -> pd.Series:
+        people_vaccinated = json_data["india_dose1"]
+        people_fully_vaccinated = json_data["india_dose2"]
+        total_vaccinations = json_data["india_total_doses"]
+        date = json_data["day"]
 
         return pd.Series(
             {
@@ -29,6 +60,10 @@ class India:
             }
         )
 
+    @property
+    def date_str(self):
+        return localdate("Asia/Calcutta", force_today=True)
+
     def pipe_location(self, ds: pd.Series) -> pd.Series:
         return enrich_data(ds, "location", self.location)
 
@@ -36,7 +71,7 @@ class India:
         return enrich_data(ds, "vaccine", "Covaxin, Oxford/AstraZeneca, Sputnik V")
 
     def pipe_source(self, ds: pd.Series) -> pd.Series:
-        return enrich_data(ds, "source_url", self.source_url_ref)
+        return enrich_data(ds, "source_url", self.source_url_ref[self.source_name])
 
     def pipeline(self, ds: pd.Series) -> pd.Series:
         return (
